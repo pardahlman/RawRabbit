ff5faa5a28f02da24240e53053b48d7febff32ed
par.dahlman <par.dahlman@valtech.se>
2015-10-22 12:39:49 +0200

    Add tests for multiple calls

---
+++ src/RawRabbit.IntegrationTests/SimpleUse/MultipleRequestsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using RawRabbit.Client;
+using RawRabbit.IntegrationTests.TestMessages;
+using Xunit;
+
+namespace RawRabbit.IntegrationTests.SimpleUse
+{
+	public class MultipleRequestsTests
+	{
+		[Fact]
+		public async void Should_Just_Work()
+		{
+			/* Setup */
+			const int numberOfCalls = 1000;
+			var bag = new ConcurrentBag<Guid>();
+			var requester = BusClientFactory.CreateDefault();
+			var responder = BusClientFactory.CreateDefault();
+			await responder.RespondAsync<FirstRequest, FirstResponse>((req, i) =>
+				Task.FromResult(new FirstResponse { Infered = Guid.NewGuid() })
+			);
+
+			/* Test */
+			var sw = new Stopwatch();
+			sw.Start();
+			for (var i = 0; i < numberOfCalls; i++)
+			{
+				var response = await requester.RequestAsync<FirstRequest, FirstResponse>();
+				bag.Add(response.Infered);
+			}
+			sw.Stop();
+
+			/* Assert */
+			Assert.Equal(numberOfCalls, bag.Count);
+		}
+	}
+}
