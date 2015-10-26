adece9af66629e8a2cae85bb7040546304871633
par.dahlman <par.dahlman@valtech.se>
2015-10-26 19:55:40 +0100

    Implement ConfigurationParser

---
+++ src/RawRabbit.Tests/Common/ConfigurationBuilderTests.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Framework.Configuration;
+using RawRabbit.Common;
+using RawRabbit.Configuration.Exchange;
+using Xunit;
+
+namespace RawRabbit.Tests.Common
+{
+	public class ConfigurationBuilderTests
+	{
+		private readonly ConfigurationParser _parser;
+		private IConfigurationBuilder _cfgBuilder;
+
+		public ConfigurationBuilderTests()
+		{
+			_cfgBuilder = new ConfigurationBuilder()
+				.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+				{
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:Hostname", "localhost"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:Password", "guest"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:UserName", "guest"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:VirtualHost", "/"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:Hostname", "production"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:Password", "admin"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:UserName", "admin"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:VirtualHost", "/prod"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Exchange:AutoDelete", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Exchange:Durable", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Exchange:Type", "Topic"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Queue:AutoDelete", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Queue:Durable", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Queue:Exclusive", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:RequestTimeout", "00:02:00")
+				});
+			_parser = new ConfigurationParser();
+		}
+
+		[Theory]
+		[InlineData("01:02:03", 1,2,3)]
+		[InlineData("05:30", 0,5,30)]
+		public void Should_Be_Able_To_Parse_Request_Timeout(string configValue, int hours, int minutes, int seconds)
+		{
+			/* Setup */
+			var expected = new TimeSpan(hours, minutes, seconds);
+			var cfg = new ConfigurationBuilder()
+				.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+				{
+					new KeyValuePair<string, string>("Data:RawRabbit:RequestTimeout", configValue)
+				})
+				.Build();
+
+			/* Test */
+			var config = _parser.Parse(cfg);
+
+			/* Assert */
+			Assert.Equal(config.RequestTimeout, expected);
+		}
+
+		[Fact]
+		public void Should_Be_Able_To_Parse_Queue_Config()
+		{
+			/* Setup */
+			var cfg = new ConfigurationBuilder()
+				.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+				{
+					new KeyValuePair<string, string>("Data:RawRabbit:Queue:AutoDelete", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Queue:Durable", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Queue:Exclusive", "True"),
+				})
+				.Build();
+
+			/* Test */
+			var config = _parser.Parse(cfg);
+
+			/* Assert */
+			Assert.True(config.Queue.Durable);
+			Assert.True(config.Queue.AutoDelete);
+			Assert.True(config.Queue.Exclusive);
+		}
+
+		[Fact]
+		public void Should_Be_Able_To_Parse_Exchange_Config()
+		{
+			/* Setup */
+			var cfg = new ConfigurationBuilder()
+				.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+				{
+					new KeyValuePair<string, string>("Data:RawRabbit:Exchange:AutoDelete", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Exchange:Durable", "True"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Exchange:Type", "Topic"),
+				})
+				.Build();
+
+			/* Test */
+			var config = _parser.Parse(cfg);
+
+			/* Assert */
+			Assert.True(config.Exchange.Durable);
+			Assert.True(config.Exchange.AutoDelete);
+			Assert.Equal(config.Exchange.Type, ExchangeType.Topic);
+		}
+
+		[Fact]
+		public void Should_Be_Able_To_Parse_Broker_Config()
+		{
+			/* Setup */
+			var cfg = new ConfigurationBuilder()
+				.AddInMemoryCollection(new List<KeyValuePair<string, string>>
+				{
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:Hostname", "localhost"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:Password", "guest"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:UserName", "guest"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:0:VirtualHost", "/"),
+					
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:Hostname", "production"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:VirtualHost", "/"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:Password", "admin"),
+					new KeyValuePair<string, string>("Data:RawRabbit:Brokers:1:UserName", "admin"),
+				})
+				.Build();
+
+			/* Test */
+			var config = _parser.Parse(cfg);
+
+			/* Assert */
+			Assert.Equal(config.Brokers[0].Hostname, "localhost");
+			Assert.Equal(config.Brokers[0].VirtualHost, "/");
+			Assert.Equal(config.Brokers[0].Username, "guest");
+			Assert.Equal(config.Brokers[0].Password, "guest");
+			Assert.Equal(config.Brokers[1].Hostname, "production");
+			Assert.Equal(config.Brokers[1].VirtualHost, "/");
+			Assert.Equal(config.Brokers[1].Username, "admin");
+			Assert.Equal(config.Brokers[1].Password, "admin");
+        }
+	}
+}
---
+++ src/RawRabbit/Common/ConfigurationParser.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Framework.Configuration;
+using RawRabbit.Configuration;
+using RawRabbit.Configuration.Exchange;
+
+namespace RawRabbit.Common
+{
+	public class ConfigurationParser
+	{
+		public RawRabbitConfiguration Parse(IConfigurationRoot root)
+		{
+			if (root == null)
+			{
+				throw new ArgumentNullException(nameof(root));
+			}
+			
+			var cfg = new RawRabbitConfiguration();
+
+			var timeout = root["Data:RawRabbit:RequestTimeout"];
+			if (timeout != null)
+			{
+				cfg.RequestTimeout = ParseToTimeSpan(timeout);
+			}
+			
+			bool autoDelete;
+			if(bool.TryParse(root["Data:RawRabbit:Queue:AutoDelete"], out autoDelete))
+			{
+				cfg.Queue.AutoDelete = autoDelete;
+			}
+			bool durable;
+			if (bool.TryParse(root["Data:RawRabbit:Queue:Durable"], out durable))
+			{
+				cfg.Queue.Durable = durable;
+			}
+			bool exclusive;
+			if (bool.TryParse(root["Data:RawRabbit:Queue:Exclusive"], out exclusive))
+			{
+				cfg.Queue.Exclusive = exclusive;
+			}
+			bool xcAutoDelete;
+			if (bool.TryParse(root["Data:RawRabbit:Exchange:AutoDelete"], out xcAutoDelete))
+			{
+				cfg.Exchange.AutoDelete= xcAutoDelete;
+			}
+			bool xcDurable;
+			if (bool.TryParse(root["Data:RawRabbit:Exchange:Durable"], out xcDurable))
+			{
+				cfg.Exchange.Durable = xcDurable;
+			}
+			ExchangeType type;
+			if (Enum.TryParse(root["Data:RawRabbit:Exchange:Type"], out type))
+			{
+				cfg.Exchange.Type = type;
+			}
+
+			cfg.Brokers = GetBrokers(root);
+			return cfg;
+		}
+
+		private static List<BrokerConfiguration> GetBrokers(IConfiguration root)
+		{
+			var result = new List<BrokerConfiguration>();
+			var continueParsing = true;
+			var index = 0;
+			while (continueParsing)
+			{
+				var broker = new BrokerConfiguration
+				{
+					Hostname = root[$"Data:RawRabbit:Brokers:{index}:Hostname"],
+					VirtualHost = root[$"Data:RawRabbit:Brokers:{index}:VirtualHost"],
+					Username = root[$"Data:RawRabbit:Brokers:{index}:Username"],
+					Password = root[$"Data:RawRabbit:Brokers:{index}:Password"]
+				};
+				if (string.IsNullOrWhiteSpace(broker.Hostname))
+				{
+					continueParsing = false;
+					continue;
+				}
+				result.Add(broker);
+				index++;
+			}
+			return result;
+		}
+
+		private static TimeSpan ParseToTimeSpan(string timeout)
+		{
+			var parts = timeout
+				.Split(':')
+				.Select(int.Parse)
+				.ToList();
+
+			if (parts.Count == 2)
+			{
+				return new TimeSpan(0,parts[0], parts[1]);
+			}
+			if (parts.Count == 3)
+			{
+				return new TimeSpan(parts[0], parts[1], parts[2]);
+			}
+			if (parts.Count == 4)
+			{
+				return new TimeSpan(parts[0], parts[1], parts[2], parts[3]);
+			}
+			throw new Exception("Unable to parse timeout");
+		}
+	}
+}
