0401eeef0ad5d6cb0cea29550ff0ac6bdbb7c2d6
par.dahlman <par.dahlman@valtech.se>
2015-11-15 12:32:00 +0100

    Add test for multiple pub/sub

--- src/RawRabbit.IntegrationTests/SimpleUse/PublishAndSubscribeTests.cs
+++ src/RawRabbit.IntegrationTests/SimpleUse/PublishAndSubscribeTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using RawRabbit.Common;
 using RawRabbit.IntegrationTests.TestMessages;
@@ -40,6 +42,38 @@ namespace RawRabbit.IntegrationTests.SimpleUse
 
 			/* Assert */
 			Assert.Equal(recievedTcs.Task.Result.Prop, message.Prop);
+		}
+
+		[Fact]
+		public async void Should_Be_Able_To_Perform_Multiple_Pub_Subs()
+		{
+			/* Setup */
+			var subscriber = BusClientFactory.CreateDefault();
+			var publisher = BusClientFactory.CreateDefault();
+			const int numberOfCalls = 100;
+			var recived = 0;
+			var recievedTcs = new TaskCompletionSource<bool>();
+			subscriber.SubscribeAsync<BasicMessage>((message, context) =>
+			{
+				Interlocked.Increment(ref recived);
+				if (numberOfCalls == recived)
+				{
+					recievedTcs.SetResult(true);
+				}
+				return Task.FromResult(true);
+			});
+
+			/* Test */
+			var sw = Stopwatch.StartNew();
+			for (int i = 0; i < numberOfCalls; i++)
+			{
+				publisher.PublishAsync<BasicMessage>();
+			}
+			await recievedTcs.Task;
+			sw.Stop();
+
+			/* Assert */
+			Assert.True(true, $"Completed {numberOfCalls} in {sw.ElapsedMilliseconds} ms.");
 		}
 	}
 }
