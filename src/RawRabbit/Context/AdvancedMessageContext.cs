6943c1ef610f091caedda532fafa68694ae3922a
par.dahlman <par.dahlman@valtech.se>
2015-11-28 21:51:06 +0100

    Add retry method to IAdvancedContext

--- src/RawRabbit/Context/AdvancedMessageContext.cs
+++ src/RawRabbit/Context/AdvancedMessageContext.cs
@@ -5,5 +5,6 @@ namespace RawRabbit.Context
 	public class AdvancedMessageContext : MessageContext, IAdvancedMessageContext
 	{
 		public Action Nack { get; set; }
+		public Action<TimeSpan> RetryLater { get; set; }
 	}
 }
--- src/RawRabbit/Context/IAdvancedMessageContext.cs
+++ src/RawRabbit/Context/IAdvancedMessageContext.cs
@@ -5,5 +5,6 @@ namespace RawRabbit.Context
 	public interface IAdvancedMessageContext : IMessageContext
 	{
 		Action Nack { get; set; }
+		Action<TimeSpan> RetryLater { get; set; }
 	}
 }
