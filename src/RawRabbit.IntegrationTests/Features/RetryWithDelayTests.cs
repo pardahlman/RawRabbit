8470cc5f131deced94cd7480ffedc3ef0d1a7228
par.dahlman <par.dahlman@valtech.se>
2015-11-28 21:52:05 +0100

    Add enhancement for retry

---
+++ src/RawRabbit.IntegrationTests/Features/RetryWithDelayTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using RawRabbit.Context;
+using RawRabbit.IntegrationTests.TestMessages;
+using RawRabbit.vNext;
+using Xunit;
+
+namespace RawRabbit.IntegrationTests.Features
+{
+	public class RetryWithDelayTests
+	{
+		[Fact]
+		public async Task Should_Retry_After_Given_Timespan()
+		{
+			/* Setup */
+			var service = new ServiceCollection()
+				.AddRawRabbit<AdvancedMessageContext>()
+				.BuildServiceProvider();
+
+			var subscriber = service.GetService<IBusClient<AdvancedMessageContext>>();
+			var publisher = service.GetService<IBusClient<AdvancedMessageContext>>();
+
+			var subscribeTcs = new TaskCompletionSource<bool>();
+			var deplay = TimeSpan.FromSeconds(1);
+			var hasBeenDelayed = false;
+			var firstRecieved = DateTime.MinValue;
+			var secondRecieved = DateTime.MinValue;
+
+			subscriber.SubscribeAsync<BasicMessage>((message, context) =>
+			{
+				if (!hasBeenDelayed)
+				{
+					firstRecieved = DateTime.Now;
+					context.RetryLater(deplay);
+					hasBeenDelayed = true;
+					return Task.FromResult(true);
+				}
+				secondRecieved = DateTime.Now;
+				return Task.Delay(10).ContinueWith(t => subscribeTcs.SetResult(true));
+			});
+
+			/* Test */
+			await publisher.PublishAsync(new BasicMessage { Prop = "I'm about to be reborn!"});
+			await subscribeTcs.Task;
+			var actualDelay = secondRecieved - firstRecieved;
+			
+			/* Assert */
+			Assert.Equal(actualDelay.Seconds, deplay.Seconds);
+		}
+	}
+}
--- src/RawRabbit/Context/Enhancer/ContextEnhancer.cs
+++ src/RawRabbit/Context/Enhancer/ContextEnhancer.cs
@@ -1,11 +1,25 @@
-﻿using RabbitMQ.Client.Events;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RawRabbit.Common;
 using RawRabbit.Consumer.Contract;
 
 namespace RawRabbit.Context.Enhancer
 {
 	public class ContextEnhancer : IContextEnhancer
 	{
-		public void WireUpContextFeatures<TMessageContext1>(TMessageContext1 context, IRawConsumer consumer, BasicDeliverEventArgs args) where TMessageContext1 : IMessageContext
+		private readonly IChannelFactory _channelFactory;
+		private readonly INamingConvetions _convetions;
+
+		public ContextEnhancer(IChannelFactory channelFactory, INamingConvetions convetions)
+		{
+			_channelFactory = channelFactory;
+			_convetions = convetions;
+		}
+
+		public void WireUpContextFeatures<TMessageContext>(TMessageContext context, IRawConsumer consumer, BasicDeliverEventArgs args) where TMessageContext : IMessageContext
 		{
 			if (context == null)
 			{
@@ -19,6 +33,28 @@ namespace RawRabbit.Context.Enhancer
 				{
 					consumer.NackedDeliveryTags.Add(args.DeliveryTag);
 					consumer.Model.BasicNack(args.DeliveryTag, false, true);
+				};
+
+				advancedCtx.RetryLater = timespan =>
+				{
+					var dlxName = _convetions.DeadLetterExchangeNamingConvention();
+					var dlQueueName = _convetions.RetryQueueNamingConvention();
+					var channel = _channelFactory.CreateChannel();
+					channel.ExchangeDeclare(dlxName, ExchangeType.Direct);
+					channel.QueueDeclare(dlQueueName, false, false, true, new Dictionary<string, object>
+					{
+						{"x-dead-letter-exchange", args.Exchange},
+						{"x-message-ttl", Convert.ToInt32(timespan.TotalMilliseconds)}
+					});
+					channel.QueueBind(dlQueueName, dlxName, args.RoutingKey, null);
+					channel.BasicPublish(dlxName, args.RoutingKey, args.BasicProperties, args.Body);
+					Timer disposeChannel = null;
+					disposeChannel = new Timer(state =>
+					{
+						channel.QueueDelete(dlQueueName); //TODO: investigate why auto-delete doesn't work?
+						channel.Dispose();
+						disposeChannel?.Dispose();
+					}, null, timespan, new TimeSpan(-1));
 				};
 			}
 		}
