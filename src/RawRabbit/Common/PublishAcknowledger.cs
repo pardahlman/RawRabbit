ad1fc22b0080e025f8eae75b0b88f679561a45db
par.dahlman <par.dahlman@valtech.se>
2015-12-03 00:05:24 +0100

    Update PublishACknowledger
    So that it uses the channel counter for next delivery tag, and looks for
    the multiple flag, too.

--- src/RawRabbit/Common/PublishAcknowledger.cs
+++ src/RawRabbit/Common/PublishAcknowledger.cs
@@ -3,6 +3,7 @@ using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using RawRabbit.Exceptions;
 using RawRabbit.Logging;
 
@@ -32,7 +33,8 @@ namespace RawRabbit.Common
 		private readonly TimeSpan _publishTimeout;
 		private readonly ILogger _logger = LogManager.GetLogger<PublishAcknowledger>();
 		private ConcurrentDictionary<ulong, TaskCompletionSource<ulong>> _deliveredAckDictionary;
-		private ulong _currentDeliveryTag;
+		private ConcurrentDictionary<ulong, Timer> _ackTimers;
+		private IModel _channel;
 
 		public PublishAcknowledger(TimeSpan publishTimeout)
 		{
@@ -42,45 +44,67 @@ namespace RawRabbit.Common
 		public void SetActiveChannel(IModel channel)
 		{
 			_deliveredAckDictionary = new ConcurrentDictionary<ulong, TaskCompletionSource<ulong>>();
-			_currentDeliveryTag = 0;
-			channel.BasicAcks += (sender, args) =>
+			_ackTimers = new ConcurrentDictionary<ulong, Timer>();
+			_channel = channel;
+			_channel.BasicAcks += (sender, args) =>
 			{
-				TaskCompletionSource<ulong> tcs;
-				if (_deliveredAckDictionary.TryRemove(args.DeliveryTag, out tcs))
+				_logger.LogInformation($"Recieved ack for {args.DeliveryTag} with multiple set to '{args.Multiple}'");
+				if (args.Multiple)
 				{
-					_logger.LogDebug($"Message with delivery tag {args.DeliveryTag} has been acknowledged by broker.");
-					tcs.TrySetResult(args.DeliveryTag);
+					_logger.LogInformation($"Woop woop, is multiple {args.DeliveryTag}");
+					for (var i = args.DeliveryTag; i > 0; i--)
+					{
+						DisposeTimer(i);
+					}
 				}
 				else
 				{
-					_logger.LogWarning($"Delivery tag {args.DeliveryTag} not found in publish acknowledger.");
+					DisposeTimer(args.DeliveryTag);
 				}
+
 			};
 			channel.ConfirmSelect();
+		}
+
+		private void DisposeTimer(ulong tag)
+		{
+			TaskCompletionSource<ulong> tcs;
+			if (_deliveredAckDictionary.TryRemove(tag, out tcs))
+			{
+				Timer ackTimer;
+				if (_ackTimers.TryRemove(tag, out ackTimer))
+				{
+					_logger.LogDebug($"Disposed ack timer for {tag}");
+					ackTimer.Dispose();
+				}
+				else
+				{
+					_logger.LogDebug($"$Unable to find ack timer for {tag}. It has probably been ack'ed allready.");
+				}
+				_logger.LogDebug($"Message with delivery tag {tag} has been acknowledged by broker.");
+				tcs.TrySetResult(tag);
+			}
 		}
 
 		public Task GetAckTask()
 		{
 			var tcs = new TaskCompletionSource<ulong>();
-			_currentDeliveryTag++;
-			if (!_deliveredAckDictionary.TryAdd(_currentDeliveryTag, tcs))
+			var nextTag = _channel.NextPublishSeqNo;
+			if (!_deliveredAckDictionary.TryAdd(nextTag, tcs))
 			{
-				_logger.LogWarning($"Unable to add delivery tag {_currentDeliveryTag} to ack list.");
+				_logger.LogWarning($"Unable to add delivery tag {nextTag} to ack list.");
 			}
-			Timer publishTimer = null;
-			publishTimer = new Timer(state =>
+			else
 			{
-				publishTimer?.Dispose();
-				TaskCompletionSource<ulong> deliveryTcs;
-				if (!_deliveredAckDictionary.TryRemove(_currentDeliveryTag, out deliveryTcs))
-				{
-					_logger.LogWarning($"Unable to find task completion source for publish ack {_currentDeliveryTag}.");
-				}
-				else
-				{
-					deliveryTcs.TrySetException(new PublishConfirmException($"The broker did not send a publish acknowledgement for message {_currentDeliveryTag} within {_publishTimeout.ToString("g")}."));
-				}
-			}, null, _publishTimeout, new TimeSpan(-1));
+				_logger.LogDebug($"Successfully added ack task for {nextTag}.");
+			}
+			_ackTimers.TryAdd(nextTag, new Timer(state =>
+			{
+				_logger.LogWarning($"Ack for {nextTag} has timed out.");
+				_ackTimers[nextTag].Dispose();
+				_deliveredAckDictionary[nextTag].TrySetException(
+						new PublishConfirmException($"The broker did not send a publish acknowledgement for message {nextTag} within {_publishTimeout.ToString("g")}."));
+			}, null, _publishTimeout, new TimeSpan(-1)));
 			return tcs.Task;
 		}
 	}
