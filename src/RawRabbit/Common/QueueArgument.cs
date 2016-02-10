c3999fcd1766945cfdbbf2c38967682425c7a85a
par.dahlman <par.dahlman@valtech.se>
2016-02-10 21:03:58 +0100

    (#44): Adding 'QueueMode' to Queue Arguments

--- src/RawRabbit/Common/QueueArgument.cs
+++ src/RawRabbit/Common/QueueArgument.cs
@@ -6,5 +6,11 @@
 		public static readonly string DeadLetterExchange = "x-dead-letter-exchange";
 		public static readonly string MessageTtl = "x-message-ttl";
 
+		/// <summary>
+		/// Set QueueMode for a queue. Valid modes are "default" and "layz".<br />
+		/// The messages for a lazy queue is held on disc and only loaded to
+		/// RAM when requested by consumer.
+		/// </summary>
+		public static readonly string QueueMode = "x-queue-mode";
 	}
 }
