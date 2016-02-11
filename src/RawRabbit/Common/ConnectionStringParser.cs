9e75d057aeb0d62b9a06dc3ecb36122eda351d2f
Marcus Utter <marcus.utter@valtech.se>
2016-02-11 14:30:50 +0100

    (#47): Adds default attributes

--- src/RawRabbit/Common/ConnectionStringParser.cs
+++ src/RawRabbit/Common/ConnectionStringParser.cs
@@ -9,7 +9,7 @@ namespace RawRabbit.Common
 {
 	public class ConnectionStringParser
 	{
-		private static readonly Regex MainRegex = new Regex(@"(?<username>.*):(?<password>.*)@(?<hosts>.*):(?<port>\d*)(?<vhost>\/[^\?]*)?(\?(?<parameters>.*))?");
+		private static readonly Regex MainRegex = new Regex(@"((?<username>.*):(?<password>.*)@)?(?<hosts>[^\/\?:]*)(:(?<port>\d+))?(?<vhost>\/[^\?]*)?(\?(?<parameters>.*))?");
 		private static readonly Regex ParametersRegex = new Regex(@"(?<name>[^?=&]+)=(?<value>[^&]*)?");
 
 		public static RawRabbitConfiguration Parse(string connectionString)
@@ -17,10 +17,10 @@ namespace RawRabbit.Common
 			var mainMatch = MainRegex.Match(connectionString);
 			var cfg = new RawRabbitConfiguration
 			{
-				Username = mainMatch.Groups["username"].Value,
-				Password = mainMatch.Groups["password"].Value,
-				VirtualHost = mainMatch.Groups["vhost"].Success ? mainMatch.Groups["vhost"].Value : "/",
-				Port = int.Parse(mainMatch.Groups["port"].Value),
+				Username = mainMatch.Groups["username"].Success ? mainMatch.Groups["username"].Value : Defaults.Username,
+				Password = mainMatch.Groups["password"].Success ? mainMatch.Groups["password"].Value : Defaults.Password,
+				VirtualHost = mainMatch.Groups["vhost"].Success ? mainMatch.Groups["vhost"].Value : Defaults.VirtualHost,
+				Port = mainMatch.Groups["port"].Success ? int.Parse(mainMatch.Groups["port"].Value) : Defaults.Port,
 				Hostnames = mainMatch.Groups["hosts"].Value.Split(',').ToList()
 			};
 
@@ -46,6 +46,14 @@ namespace RawRabbit.Common
 			}
 
 			return cfg;
+		}
+
+		private static class Defaults
+		{
+			public const string Username = "guest";
+			public const string Password = "guest";
+			public const string VirtualHost = "/";
+			public const int Port = 5672;
 		}
 	}
 }
