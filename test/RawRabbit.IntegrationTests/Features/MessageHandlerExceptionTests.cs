444efffedb4850e7146378705432b95441dc985d
par.dahlman <par.dahlman@valtech.se>
2016-02-23 00:34:58 +0100

    (#60) Use ContinueWith rather than Wait for Consumer
    This increases the through put by a lot :)

--- test/RawRabbit.IntegrationTests/Features/MessageHandlerExceptionTests.cs
+++ test/RawRabbit.IntegrationTests/Features/MessageHandlerExceptionTests.cs
@@ -10,7 +10,25 @@ namespace RawRabbit.IntegrationTests.Features
 	public class MessageHandlerExceptionTests
 	{
 		[Fact]
-		public async Task Should_Throw_Exception_To_Requester_If_Responder_Throws()
+		public async Task Should_Throw_Exception_To_Requester_If_Responder_Throws_Async()
+		{
+			/* Setup */
+			var responseException = new NotSupportedException("I'll throw this");
+			var requester = BusClientFactory.CreateDefault(TimeSpan.FromHours(1));
+			var responder = BusClientFactory.CreateDefault(TimeSpan.FromHours(1));
+			responder.RespondAsync<BasicRequest, BasicResponse>(async (request, context) =>
+			{
+				throw responseException;
+			});
+
+			/* Test */
+			/* Assert */
+			var e = await Assert.ThrowsAsync<MessageHandlerException>(() => requester.RequestAsync<BasicRequest, BasicResponse>());
+			Assert.Equal(e.InnerException.Message, responseException.Message);
+		}
+
+		[Fact]
+		public async Task Should_Throw_Exception_To_Requester_If_Responder_Throws_Sync()
 		{
 			/* Setup */
 			var responseException = new NotSupportedException("I'll throw this");
@@ -23,7 +41,7 @@ namespace RawRabbit.IntegrationTests.Features
 
 			/* Test */
 			/* Assert */
-			var e = await Assert.ThrowsAsync<MessageHandlerException>(async () => await requester.RequestAsync<BasicRequest, BasicResponse>());
+			var e = await Assert.ThrowsAsync<MessageHandlerException>(() => requester.RequestAsync<BasicRequest, BasicResponse>());
 			Assert.Equal(e.InnerException.Message, responseException.Message);
 		}
 	}
