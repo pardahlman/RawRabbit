e49b4734d0bd7990e00edc868ccf991549864869
Marcus Utter <marcus.utter@valtech.se>
2016-03-03 15:45:11 +0100

    (#65) Serializes Message Type without assembly version etc

--- src/RawRabbit/Common/BasicPropertiesProvider.cs
+++ src/RawRabbit/Common/BasicPropertiesProvider.cs
@@ -30,8 +30,28 @@ namespace RawRabbit.Common
 			};
 			custom?.Invoke(properties);
 			properties.Headers.Add(PropertyHeaders.Sent, DateTime.UtcNow.ToString("u"));
-			properties.Headers.Add(PropertyHeaders.MessageType, $"{typeof(TMessage).FullName}, {typeof(TMessage).Assembly.GetName().Name}");
+			properties.Headers.Add(PropertyHeaders.MessageType, GetTypeName(typeof(TMessage)));
 			return properties;
+		}
+
+		public string GetTypeName(Type type)
+		{
+			var name = $"{type.Namespace}.{type.Name}";
+			if (type.GenericTypeArguments.Length > 0)
+			{
+				var shouldInsertComma = false;
+				name += '[';
+				foreach (var genericType in type.GenericTypeArguments)
+				{
+					if (shouldInsertComma)
+						name += ",";
+					name += $"[{GetTypeName(genericType)}]";
+					shouldInsertComma = true;
+				}
+				name += ']';
+			}
+			name += $", {type.Assembly.GetName().Name}";
+			return name;
 		}
 	}
 }
