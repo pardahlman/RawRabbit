698d4c4474924f57377da981a7efb47b3baa0b73
videege <jbarron@barronsoftware.com>
2016-03-15 22:14:29 -0600

    (feature) Adding new RawRabbit.DependencyInjection.Ninject extension package for using RawRabbit with the Ninject container

---
+++ src/RawRabbit.DependencyInjection.Ninject/KernelExtension.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ninject;
+using RawRabbit.Common;
+using RawRabbit.Configuration;
+using RawRabbit.Context;
+
+namespace RawRabbit.DependencyInjection.Ninject
+{
+    public static class KernelExtension
+    {
+        public static IKernel RegisterRawRabbit<TMessageContext>(this IKernel kernel)
+            where TMessageContext : IMessageContext
+        {
+            kernel.Load(new RawRabbitModule<TMessageContext>());
+            return kernel;
+        }
+
+        public static IKernel RegisterRawRabbit(this IKernel kernel)
+        {
+            kernel.Load(new RawRabbitModule());
+            return kernel;
+        }
+
+        public static IKernel RegisterRawRabbit<TMessageContext>(this IKernel kernel, RawRabbitConfiguration configuration)
+            where TMessageContext : IMessageContext
+        {
+            kernel
+                .Bind<RawRabbitConfiguration>()
+                .ToConstant(configuration)
+                .InSingletonScope();
+
+            kernel.Load(new RawRabbitModule<TMessageContext>());
+            return kernel;
+        }
+
+        public static IKernel RegisterRawRabbit(this IKernel kernel, RawRabbitConfiguration configuration)
+        {
+            return RegisterRawRabbit<MessageContext>(kernel, configuration);
+        }
+
+        public static IKernel RegisterRawRabbit<TMessageContext>(this IKernel kernel, string connectionString)
+            where TMessageContext : IMessageContext
+        {
+            var config = ConnectionStringParser.Parse(connectionString);
+            return RegisterRawRabbit<TMessageContext>(kernel, config);
+        }
+
+        public static IKernel RegisterRawRabbit(this IKernel kernel, string connectionString)
+        {
+            return RegisterRawRabbit<MessageContext>(kernel, connectionString);
+        }
+
+    }
+}
---
+++ test/RawRabbit.Tests/DependencyInjection/NinjectTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ninject;
+using RawRabbit.Context;
+using Xunit;
+using RawRabbit.DependencyInjection.Ninject;
+
+namespace RawRabbit.Tests.DependencyInjection
+{
+    public class NinjectTests
+    {
+        [Fact]
+        public void Should_Be_Able_To_Resolve_IBusClient()
+        {
+            /* Setup */
+            var kernel = new StandardKernel();
+            kernel.RegisterRawRabbit("guest:guest@localhost:5672/");
+            
+            /* Test */
+            var client = kernel.Get<IBusClient>();
+
+            /* Assert */
+            Assert.True(true, "Could resolve");
+        }
+
+        [Fact]
+        public void Should_Be_Able_To_Resolve_BusClient_With_Advanced_Context()
+        {
+            /* Setup */
+            var kernel = new StandardKernel();
+            kernel.RegisterRawRabbit<AdvancedMessageContext>("guest:guest@localhost:5672/");
+
+            /* Test */
+            var client = kernel.Get<IBusClient<AdvancedMessageContext>>();
+
+            /* Assert */
+            Assert.True(true, "Could resolve");
+        }
+    }
+}
