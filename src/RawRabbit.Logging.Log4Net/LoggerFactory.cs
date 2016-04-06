a5176178166c53c41172d673d5d393028e585355
par.dahlman <par.dahlman@valtech.se>
2016-04-06 07:35:13 +0200

    (#77) Add logger package for log4net

---
+++ src/RawRabbit.Logging.Log4Net/LoggerFactory.cs
@@ -0,0 +1,15 @@
+namespace RawRabbit.Logging.Log4Net
+{
+	public class LoggerFactory : ILoggerFactory {
+
+		public LogLevel MinimumLevel { get; set; }
+
+		public ILogger CreateLogger(string categoryName)
+		{
+			var log4net = global::log4net.LogManager.GetLogger(categoryName);
+			return new Logger(log4net);
+		}
+
+		public void Dispose() { }
+	}
+}
---
+++ src/RawRabbit.Logging.Log4Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RawRabbit.Logging.Log4Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Valtech AB")]
+[assembly: AssemblyProduct("RawRabbit.Logging.Log4Net")]
+[assembly: AssemblyCopyright("Copyright © Valtech AB 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9385228c-444e-443e-b1e8-bd88cafa508b")]
