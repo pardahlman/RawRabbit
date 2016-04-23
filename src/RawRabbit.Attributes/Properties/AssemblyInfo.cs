b68cf973fac6e560aa0ed06b409e1a75b1048a11
par.dahlman <par.dahlman@valtech.se>
2016-04-23 09:41:42 +0200

    (#81) Add project for Attributes

---
+++ src/RawRabbit.Attributes/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RawRabbit.Attributes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Valtech AB")]
+[assembly: AssemblyProduct("RawRabbit.Attributes")]
+[assembly: AssemblyCopyright("Copyright © Valtech AB 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7edebe9-230e-493b-8f7a-75a15091e7c8")]
---
+++ test/RawRabbit.IntegrationTests/Attributes/TopologyAttributeTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using RawRabbit.Attributes;
+using RawRabbit.Common;
+using RawRabbit.Configuration.Exchange;
+using RawRabbit.vNext;
+using Xunit;
+
+namespace RawRabbit.IntegrationTests.Attributes
+{
+	public class TopologyAttributeTests : IntegrationTestBase
+	{
+		[Fact]
+		public async Task Should_Work_For_Pub_Sub()
+		{
+			/* Setup */
+			var client = BusClientFactory.CreateDefault(ioc => ioc
+				.AddSingleton<IConfigurationEvaluator, AttributeConfigEvaluator>()
+			);
+			var tcs = new TaskCompletionSource<AttributedMessage>();
+			client.SubscribeAsync<AttributedMessage>((message, context) =>
+			{
+				tcs.TrySetResult(message);
+				return Task.FromResult(true);
+			});
+
+			/* Test */
+			await client.PublishAsync(new AttributedMessage());
+			await tcs.Task;
+
+			/* Assert */
+			Assert.True(true, "Routing successful.");
+		}
+
+		[Fact]
+		public async Task Should_Work_For_Rpc()
+		{
+			/* Setup */
+			var client = BusClientFactory.CreateDefault(ioc => ioc
+				.AddSingleton<IConfigurationEvaluator, AttributeConfigEvaluator>()
+			);
+			client.RespondAsync<AttributedRequest, AttributedResponse>((message, context) =>
+				Task.FromResult(new AttributedResponse())
+			);
+
+			/* Test */
+			await client.RequestAsync<AttributedRequest, AttributedResponse>();
+
+			/* Assert */
+			Assert.True(true, "Routing successful.");
+		}
+
+		[Fact]
+		public async Task Should_Honor_Custom_Config()
+		{
+			/* Setup */
+			var client = BusClientFactory.CreateDefault(ioc => ioc
+				.AddSingleton<IConfigurationEvaluator, AttributeConfigEvaluator>()
+			);
+			var tcs = new TaskCompletionSource<AttributedMessage>();
+			client.SubscribeAsync<AttributedMessage>((message, context) =>
+			{
+				tcs.TrySetResult(message);
+				return Task.FromResult(true);
+			}, c => c.WithRoutingKey("special").WithExchange(e => e.WithName("special")));
+
+			/* Test */
+			await client.PublishAsync(new AttributedMessage(), configuration: c => c.WithRoutingKey("special").WithExchange(e => e.WithName("special")));
+			await tcs.Task;
+
+			/* Assert */
+			Assert.True(true, "Routing successful.");
+		}
+
+		[Queue(Name = "my_queue", MessageTtl = 300, DeadLeterExchange = "dlx", Durable = false)]
+		[Exchange(Name = "my_topic", Type = ExchangeType.Topic)]
+		[Routing(RoutingKey = "my_key", NoAck = true, PrefetchCount = 50)]
+		private class AttributedMessage { }
+
+		[Queue(Name = "attributed_rpc", MessageTtl = 300, DeadLeterExchange = "dlx", Durable = false)]
+		[Exchange(Name = "my_topic", Type = ExchangeType.Topic)]
+		[Routing(RoutingKey = "my_key", NoAck = true, PrefetchCount = 50)]
+		private class AttributedRequest { }
+
+		private class AttributedResponse { }
+	}
+}
