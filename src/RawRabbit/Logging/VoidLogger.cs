519ec725eaedc114b39ab0b0e8fe209729474462
par.dahlman <par.dahlman@valtech.se>
2016-09-04 18:40:11 +0200

    Adding VoidLogger

---
+++ src/RawRabbit/Logging/VoidLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RawRabbit.Logging
+{
+	public class VoidLogger : ILogger
+	{
+		public void LogDebug(string format, params object[] args)
+		{
+		}
+
+		public void LogInformation(string format, params object[] args)
+		{
+		}
+
+		public void LogWarning(string format, params object[] args)
+		{
+		}
+
+		public void LogError(string message, Exception exception)
+		{
+		}
+	}
+}
