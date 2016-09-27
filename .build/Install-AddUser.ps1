c1ffdcb993cce0a1335e8ad8bdd30ee72649bd30
par.dahlman <par.dahlman@valtech.se>
2016-09-27 20:30:18 +0200

    (#107) Remove redundant logging

--- .build/Install-AddUser.ps1
+++ .build/Install-AddUser.ps1
@@ -3,7 +3,6 @@ write-host 'install:Add RabbitMq User "RawRabbit"'
 
 $rabbitMqPath = Get-RabbitMQPath
 $rabbitmqctl = '$rabbitMqPath\sbin\rabbitmqctl.bat'
-write-host "install: using path $rabbitmqctl for RabbitMq Command Line Tool"
 
 $createUser = "cmd.exe /C $rabbitmqctl add_user RawRabbit RawRabbit"
 $setPermission = "cmd.exe /C $rabbitmqctl set_permissions -p / RawRabbit `".*`" `".*`" `".*`""
