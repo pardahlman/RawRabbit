e7d249ceed7dd92ee5177837725e8a5ef8967007
par.dahlman <par.dahlman@valtech.se>
2016-09-29 21:57:06 +0200

    (#105) Add health check before adding user

--- .build/Install-AddUser.ps1
+++ .build/Install-AddUser.ps1
@@ -4,8 +4,10 @@ write-host 'install:Add RabbitMq User "RawRabbit"'
 $rabbitMqPath = Get-RabbitMQPath
 $rabbitmqctl = '$rabbitMqPath\sbin\rabbitmqctl.bat'
 
+$healthCheck = "cmd.exe /C $rabbitmqctl node_health_check"
 $createUser = "cmd.exe /C $rabbitmqctl add_user RawRabbit RawRabbit"
 $setPermission = "cmd.exe /C $rabbitmqctl set_permissions -p / RawRabbit `".*`" `".*`" `".*`""
 
+Invoke-Expression -Command:$healthCheck
 Invoke-Expression -Command:$createUser
 Invoke-Expression -Command:$setPermission
