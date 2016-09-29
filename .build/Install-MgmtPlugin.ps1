c7177bd4838e8cbeef1414ae0cd9d4d37c3a6c7b
par.dahlman <par.dahlman@valtech.se>
2016-09-29 08:11:30 +0200

    (#105) Add Sleep after port test

--- .build/Install-MgmtPlugin.ps1
+++ .build/Install-MgmtPlugin.ps1
@@ -1,4 +1,4 @@
-write-host "install: verifying that RabbitMq Mgmt Tool is up"
+Write-Host "install: verifying that RabbitMq Mgmt Tool is up"
 
 $client = New-Object System.Net.Sockets.TcpClient([System.Net.Sockets.AddressFamily]::InterNetwork)
 $attempt=0
@@ -7,10 +7,11 @@ while(!$client.Connected -and $attempt -lt 30) {
 	try {
 		$attempt++;
 		$client.Connect("127.0.0.1", 15672);
-		write-host "install: mgmt tool is listening on port $port"
+		Write-Host "install: mgmt tool is listening on port"
 		}
 	catch {
-		write-host "install: mgmt tool not is listening on port $port"
+		Write-Host "install: mgmt tool not is listening on port"
+		Start-Sleep 1
 		}
 }
 $client.Close()
