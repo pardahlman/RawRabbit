56f3610800b2e3d5c84b044ae8fca8743e7544d2
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:19:22 +0200

    (#118) Add Startup class for Pipe Configuration

---
+++ src/RawRabbit/Pipe/Startup.cs
@@ -0,0 +1,48 @@
+﻿using RawRabbit.Context;
+using RawRabbit.Pipe.Middleware;
+
+namespace RawRabbit.Pipe
+{
+	public interface IStartup
+	{
+		void ConfigureSubscribe(IPipeBuilder pipe);
+		void ConfigurePublish(IPipeBuilder pipe);
+	}
+
+	public class Startup<TMessageContext> : IStartup where TMessageContext : IMessageContext
+	{
+		public void ConfigureSubscribe(IPipeBuilder pipe)
+		{
+			pipe
+				.Use<OperationConfigurationMiddleware>()
+				.Use<RoutingKeyMiddleware>()
+				.Use<QueueBindMiddleware>()
+				.Use<MessageConsumeMiddleware>(new ConsumeOptions
+				{
+					Pipe = consume => consume
+						.Use<MessageDeserializationMiddleware>()
+						.Use<ContextExtractionMiddleware<TMessageContext>>()
+						.Use<MessageChainingMiddleware>()
+						.Use<MessageContextEnhanceMiddleware>()
+						.Use<MessageInvokationMiddleware>()
+				});
+		}
+
+		public void ConfigurePublish(IPipeBuilder pipe)
+		{
+			pipe
+				.Use<OperationConfigurationMiddleware>()
+				.Use<MessageChainingMiddleware>()
+				.Use<GlobalMessageIdMiddleware>()
+				.Use<RoutingKeyMiddleware>()
+				.Use<ExchangeDeclareMiddleware>()
+				.Use<MessageContextCreationMiddleware<TMessageContext>>()
+				.Use<ContextSerializationMiddleware>()
+				.Use<BasicPropertiesMiddleware>()
+				.Use<MessageSerializationMiddleware>()
+				.Use<PublishChannelMiddleware>()
+				.Use<PublishAcknowledgeMiddleware>()
+				.Use<PublishMessage>();
+		}
+	}
+}
