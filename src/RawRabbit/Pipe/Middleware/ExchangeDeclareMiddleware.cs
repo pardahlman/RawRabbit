8b6868ba209524f69615f26262ab5d05c7cc413b
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:09:23 +0200

    (#126) Add Exchange Declare Middleware

---
+++ src/RawRabbit/Pipe/Middleware/ExchangeDeclareMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using RawRabbit.Common;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class ExchangeDeclareMiddleware : Middleware
+	{
+		private readonly ITopologyProvider _topologyProvider;
+
+		public ExchangeDeclareMiddleware(ITopologyProvider topologyProvider)
+		{
+			_topologyProvider = topologyProvider;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			var exchangeCcfg = context.GetExchangeConfiguration();
+			return _topologyProvider
+				.DeclareExchangeAsync(exchangeCcfg)
+				.ContinueWith(t => Next.InvokeAsync(context))
+				.Unwrap();
+		}
+	}
+}
