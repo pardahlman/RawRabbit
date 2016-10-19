8514da49e2d6372fd7c3198fd4e40f8f4bbac1cb
par.dahlman <par.dahlman@valtech.se>
2016-10-19 21:26:30 +0200

    (#118) Add PipeBuilder Factory class
    We will have sepearate pipes for publish, subscribe, respond and request
    (and possibly consume), so it makes sense to have a PipeBuilder.

---
+++ src/RawRabbit/Pipe/PipeBuilderFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RawRabbit.Pipe
+{
+	public interface IPipeBuilderFactory
+	{
+		IExtendedPipeBuilder Create();
+	}
+
+	public class PipeBuilderFactory : IPipeBuilderFactory
+	{
+		private readonly Func<IExtendedPipeBuilder> _pipeBuilder;
+
+		public PipeBuilderFactory(Func<IExtendedPipeBuilder> pipeBuilder)
+		{
+			_pipeBuilder = pipeBuilder;
+		}
+
+		public IExtendedPipeBuilder Create()
+		{
+			return _pipeBuilder();
+		}
+	}
+}
