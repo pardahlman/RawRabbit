b14aa504190a61d6b62384b188b99eb87d9cfc7f
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:15:37 +0200

    (#118) Add User Handler Middleware
    That registeres a lambda, for the few times when you just want to add
    something small to your pipe.

---
+++ src/RawRabbit/Pipe/Middleware/UseHandlerMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using RawRabbit.Configuration;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class UseHandlerMiddleware : Middleware
+	{
+		private readonly Func<IPipeContext, Func<Task>, Task> _handler;
+
+		public UseHandlerMiddleware(Func<IPipeContext, Func<Task>, Task> handler)
+		{
+			_handler = handler;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			return _handler(context, () => Next.InvokeAsync(context));
+		}
+	}
+}
