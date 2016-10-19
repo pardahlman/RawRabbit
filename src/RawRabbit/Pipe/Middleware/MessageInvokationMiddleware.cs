143d3c45ae4cdbe7113d496aa55a04eb71d4fd48
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:19:02 +0200

    (#127) Add Message Invokation Middleware

---
+++ src/RawRabbit/Pipe/Middleware/MessageInvokationMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class MessageInvokationMiddleware : Middleware
+	{
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			var msgContext = context.GetMessageContext();
+			var message = context.GetMessage();
+			var handler = context.GetMessageHandler();
+
+			return handler
+				.Invoke(message, msgContext)
+				.ContinueWith(t => Next.InvokeAsync(context))
+				.Unwrap();
+		}
+	}
+}
