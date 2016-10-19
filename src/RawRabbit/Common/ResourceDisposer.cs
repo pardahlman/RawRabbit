bf1e71b1674c32092680126e77d4e312de12800b
par.dahlman <par.dahlman@valtech.se>
2016-10-19 21:32:44 +0200

    (#112) Add Resource Disposer
    DRAFT! A class that holds references to known classes that may alocate
    resources in different threads and hinder shutdown

---
+++ src/RawRabbit/Common/ResourceDisposer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RabbitMQ.Client;
+using RawRabbit.Channel.Abstraction;
+
+namespace RawRabbit.Common
+{
+	public interface IResourceDisposer :IDisposable
+	{
+	}
+
+	public class ResourceDisposer : IResourceDisposer
+	{
+		private readonly IChannelFactory _channelFactory;
+		private readonly IConnectionFactory _connectionFactory;
+
+		public ResourceDisposer(IChannelFactory channelFactory, IConnectionFactory connectionFactory)
+		{
+			_channelFactory = channelFactory;
+			_connectionFactory = connectionFactory;
+		}
+
+		public void Dispose()
+		{
+			_channelFactory.Dispose();
+			(_connectionFactory as IDisposable)?.Dispose();
+		}
+	}
+}
