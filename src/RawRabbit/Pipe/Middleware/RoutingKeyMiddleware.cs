989929076dc885fc368c02861090d43db26deab8
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:08:07 +0200

    (#126) Middleware for Routing Key

---
+++ src/RawRabbit/Pipe/Middleware/RoutingKeyMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using RawRabbit.Common;
+using RawRabbit.Configuration;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class RoutingKeyMiddleware : Middleware
+	{
+		private readonly INamingConventions _conventions;
+		private readonly RawRabbitConfiguration _config;
+
+		public RoutingKeyMiddleware(INamingConventions conventions, RawRabbitConfiguration config)
+		{
+			_conventions = conventions;
+			_config = config;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			string routingKey;
+			var operation = context.GetOperation();
+
+			if (operation == Operation.Unknown)
+			{
+				throw new Exception();
+			}
+
+			switch (operation)
+			{
+				case Operation.Publish:
+					routingKey = GetPublishRoutingKey(context);
+					break;
+				case Operation.Subscribe:
+					routingKey = GetSubscribeRoutingKey(context);
+					break;
+				default:
+					routingKey = string.Empty;
+					break;
+			}
+
+			context.Properties.Add(PipeKey.RoutingKey, routingKey);
+			return Next.InvokeAsync(context);
+		}
+
+		private string GetSubscribeRoutingKey(IPipeContext context)
+		{
+			var msgType = context.GetMessageType();
+			var routingKey = _conventions.RoutingKeyConvention(msgType);
+			return _config.RouteWithGlobalId
+				? $"{routingKey}.#"
+				: routingKey;
+		}
+
+		private string GetPublishRoutingKey(IPipeContext context)
+		{
+			var msgType = context.GetMessageType();
+			var msgId = context.GetGlobalMessageId();
+			var routingKey = _conventions.RoutingKeyConvention(msgType);
+			return _config.RouteWithGlobalId
+				? $"{routingKey}.{msgId}"
+				: routingKey;
+		}
+	}
+}
