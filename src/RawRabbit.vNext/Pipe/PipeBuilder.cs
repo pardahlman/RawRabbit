62fc48b8c100b3b6e7b3c1f7cf5ade0047489f08
par.dahlman <par.dahlman@valtech.se>
2016-10-19 21:21:38 +0200

    (#118) Add PipeBuilder with ServiceProvider capabilities
    So that the middleware can inject dependencies

---
+++ src/RawRabbit.vNext/Pipe/PipeBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using RawRabbit.Pipe.Middleware;
+
+namespace RawRabbit.vNext.Pipe
+{
+	public class PipeBuilder : RawRabbit.Pipe.PipeBuilder
+	{
+		private readonly IServiceProvider _provider;
+
+		public PipeBuilder(IServiceProvider provider)
+		{
+			_provider = provider;
+		}
+
+		protected override Middleware CreateInstance(MiddlewareInfo middlewareInfo)
+		{
+			return ActivatorUtilities.CreateInstance(_provider, middlewareInfo.Type, middlewareInfo.ConstructorArgs) as Middleware;
+		}
+	}
+}
