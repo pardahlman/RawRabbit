6a865f76973dbc02b89210a04a44cb5fc76db249
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:09:54 +0200

    (#126) Add Publish Message Middleware

---
+++ src/RawRabbit/Pipe/Middleware/PublishMessage.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using RawRabbit.Channel.Abstraction;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class PublishMessage : Middleware
+	{
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			var exchange = context.GetExchangeConfiguration();
+			var routingKey = context.GetRoutingKey();
+			var basicProps = context.GetBasicProperties();
+			var body = context.Get<byte[]>(PipeKey.MessageBytes);
+			var channel = context.GetChannel();
+
+			channel.BasicPublish(
+				exchange: exchange.ExchangeName,
+				routingKey: routingKey,
+				basicProperties: basicProps,
+				body: body,
+				mandatory: false
+				);
+
+			return Next.InvokeAsync(context);
+		}
+	}
+}
