4264b43fd365c0397e27f99957259860e2003f08
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:36:27 +0200

    (#120) Implement Publish Confirm Timeout

--- src/RawRabbit/Pipe/Middleware/PublishAcknowledgeMiddleware.cs
+++ src/RawRabbit/Pipe/Middleware/PublishAcknowledgeMiddleware.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using RabbitMQ.Client.Events;
 using RawRabbit.Common;
+using RawRabbit.Configuration;
+using RawRabbit.Exceptions;
 
 namespace RawRabbit.Pipe.Middleware
 {
 	public class PublishAcknowledgeMiddleware : Middleware
 	{
+		private TimeSpan _publishTimeOut;
+
+		public PublishAcknowledgeMiddleware(RawRabbitConfiguration config)
+		{
+			_publishTimeOut = config.PublishConfirmTimeout;
+		}
 		public override Task InvokeAsync(IPipeContext context)
 		{
 			var channel = context.GetChannel();
@@ -17,6 +26,14 @@ namespace RawRabbit.Pipe.Middleware
 
 			var thisSequence = channel.NextPublishSeqNo;
 			var ackTcs = new TaskCompletionSource<ulong>();
+			Timer ackTimer = null;
+			ackTimer = new Timer(state =>
+			{
+				ackTcs.TrySetException(
+					new PublishConfirmException(
+						$"The broker did not send a publish acknowledgement for message {thisSequence} on channel {channel.ChannelNumber} within {_publishTimeOut.ToString("g")}."));
+				ackTimer?.Dispose();
+			}, channel, _publishTimeOut, new TimeSpan(-1));
 
 			EventHandler<BasicAckEventArgs> channelBasicAck = null;
 			channelBasicAck = (sender, args) =>
