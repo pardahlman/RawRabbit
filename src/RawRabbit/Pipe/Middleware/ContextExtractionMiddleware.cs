c068c39581f6ff83180d361543af3a17a9225df7
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:12:38 +0200

    (#124) Add Context Extraction Middleware
    DRAFT! This is the 'make it work' version of things.

---
+++ src/RawRabbit/Pipe/Middleware/ContextExtractionMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using RawRabbit.Common;
+using RawRabbit.Context;
+using RawRabbit.Context.Provider;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class ContextExtractionMiddleware<TMessageContext> : Middleware where TMessageContext : IMessageContext
+	{
+		private readonly IMessageContextProvider<TMessageContext> _contextProvider;
+
+		public ContextExtractionMiddleware(IMessageContextProvider<TMessageContext> contextProvider)
+		{
+			_contextProvider = contextProvider;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			var basicProperties = context.GetBasicProperties();
+			var msgContext = _contextProvider.ExtractContext(basicProperties?.Headers?.GetOrDefault(PropertyHeaders.Context));
+			context.Properties.Add(PipeKey.MessageContext, msgContext);
+			return Next.InvokeAsync(context);
+		}
+	}
+}
