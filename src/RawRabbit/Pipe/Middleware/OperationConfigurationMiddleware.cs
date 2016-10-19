b6304c0995f531c52370c76aa8830190a36033ba
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:01:51 +0200

    (#123) Middleware for Extracting Operation Config

---
+++ src/RawRabbit/Pipe/Middleware/OperationConfigurationMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using RawRabbit.Common;
+using RawRabbit.Configuration.Publish;
+using RawRabbit.Configuration.Respond;
+using RawRabbit.Configuration.Subscribe;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class OperationConfigurationMiddleware : Middleware
+	{
+		private readonly IConfigurationEvaluator _configEvaluator;
+
+		public OperationConfigurationMiddleware(IConfigurationEvaluator configEvaluator)
+		{
+			_configEvaluator = configEvaluator;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			var operation = context.GetOperation();
+			var msgType = context.GetMessageType();
+
+			switch (operation)
+			{
+				case Operation.Subscribe:
+					{
+						var action = context.Get<Action<ISubscriptionConfigurationBuilder>>(PipeKey.ConfigurationAction);
+						IConsumerConfiguration cfg = _configEvaluator.GetConfiguration(msgType, action);
+						context.Properties.Add(PipeKey.QueueConfiguration, cfg.Queue);
+						context.Properties.Add(PipeKey.ExchangeConfiguration, cfg.Exchange);
+						context.Properties.Add(PipeKey.NoAck, cfg.NoAck);
+						context.Properties.Add(PipeKey.PrefetchCount, cfg.PrefetchCount);
+					}
+					break;
+
+				case Operation.Publish:
+					{
+						var action = context.Get<Action<IPublishConfigurationBuilder>>(PipeKey.ConfigurationAction);
+						var cfg = _configEvaluator.GetConfiguration(msgType, action);
+						context.Properties.Add(PipeKey.ExchangeConfiguration, cfg.Exchange);
+						context.Properties.Add(PipeKey.BasicPropertyModifier, cfg.PropertyModifier);
+					}
+					break;
+				default:
+					break;
+			}
+
+			context.Properties.Remove(PipeKey.ConfigurationAction);
+			return Next.InvokeAsync(context);
+		}
+	}
+}
