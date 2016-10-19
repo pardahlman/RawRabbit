8eed11ec65338effe1f709b6fa04a0b6d6bc8980
par.dahlman <par.dahlman@valtech.se>
2016-10-19 21:50:22 +0200

    (#119) Add Middleware for Publish Channel
    DRAFT! Might to something more fancy later on.

---
+++ src/RawRabbit/Pipe/Middleware/PublishChannelMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using RawRabbit.Channel.Abstraction;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class PublishChannelMiddleware : Middleware
+	{
+		private readonly IChannelFactory _channelFactory;
+
+		public PublishChannelMiddleware(IChannelFactory channelFactory)
+		{
+			_channelFactory = channelFactory;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			return _channelFactory
+				.GetChannelAsync()
+				.ContinueWith(async tChannel =>
+				{
+					context.Properties.Add(PipeKey.Channel, tChannel.Result);
+					await Next.InvokeAsync(context);
+				});
+		}
+	}
+}
