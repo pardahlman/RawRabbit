db81041c7ea0f69c42736f188751366cdc0097a2
par.dahlman <par.dahlman@valtech.se>
2016-10-19 22:16:59 +0200

    (#127) Add Midddleware for binding Queue

---
+++ src/RawRabbit/Pipe/Middleware/QueueBindMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using RawRabbit.Common;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class QueueBindMiddleware : Middleware
+	{
+		private readonly ITopologyProvider _topologyProvider;
+
+		public QueueBindMiddleware(ITopologyProvider topologyProvider)
+		{
+			_topologyProvider = topologyProvider;
+		}
+
+		public override Task InvokeAsync(IPipeContext context)
+		{
+			var consumerCfg = context.GetConsumerConfiguration();
+
+			return _topologyProvider
+				.BindQueueAsync(consumerCfg.Queue, consumerCfg.Exchange, consumerCfg.RoutingKey)
+				.ContinueWith(t => Next.InvokeAsync(context))
+				.Unwrap();
+		}
+	}
+}
