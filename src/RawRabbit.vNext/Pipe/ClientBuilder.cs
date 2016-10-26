7d6d28a98148eece9414e0e39b144d92218fbe59
par.dahlman <par.dahlman@valtech.se>
2016-10-26 08:08:58 +0200

    (#131) Add client builder + temp factory

---
+++ src/RawRabbit.vNext/Pipe/ClientBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using RawRabbit.Pipe;
+
+namespace RawRabbit.vNext.Pipe
+{
+	public interface IClientBuilder
+	{
+		void Register(Action<IPipeBuilder> pipe, Action<IServiceCollection> ioc = null);
+	}
+
+	public class ClientBuilder : IClientBuilder
+	{
+		public Action<IPipeBuilder> PipeBuilderAction { get; set; }
+		public Action<IServiceCollection> ServiceAction { get; set; }
+
+		public ClientBuilder()
+		{
+			PipeBuilderAction = builder => { };
+			ServiceAction = collection => { };
+		}
+
+		public void Register(Action<IPipeBuilder> pipe, Action<IServiceCollection> ioc)
+		{
+			PipeBuilderAction += pipe;
+			ServiceAction += ioc;
+		}
+	}
+}
---
+++ src/RawRabbit.vNext/Pipe/RawRabbitFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RawRabbit.Configuration;
+using RawRabbit.Pipe;
+
+namespace RawRabbit.vNext.Pipe
+{
+	public static class RawRabbitFactory
+	{
+		public static RawRabbit.Pipe.Client.IBusClient Create(RawRabbitOptions options = null)
+		{
+			var collection = new ServiceCollection().AddRawRabbit();
+			options?.DependencyInjection?.Invoke(collection);
+
+			if (options?.Plugins != null)
+			{
+				var clientBuilder = new ClientBuilder();
+				options.Plugins(clientBuilder);
+				clientBuilder.ServiceAction(collection);
+				collection.AddSingleton<IPipeBuilder, PipeBuilder>();
+				collection.AddSingleton(clientBuilder.PipeBuilderAction);
+			}
+			if (options?.Configuration != null)
+			{
+				var builder = new ConfigurationBuilder();
+				options.Configuration.Invoke(builder);
+				var mainCfg = RawRabbitConfiguration.Local;
+				builder.Build().Bind(mainCfg);
+				mainCfg.Hostnames = mainCfg.Hostnames.Distinct(StringComparer.CurrentCultureIgnoreCase).ToList();
+				collection.AddSingleton(c => mainCfg);
+			}
+			var provider = collection.BuildServiceProvider();
+			var pipeBuilder = new PipeBuilder(provider);
+			var pipeBuliderFactory = new PipeBuilderFactory(() => pipeBuilder);
+			return new RawRabbit.Pipe.Client.BusClient(pipeBuliderFactory, provider.GetService<IPipeContextFactory>());
+		}
+	}
+
+	public class RawRabbitOptions
+	{
+		public Action<IConfigurationBuilder> Configuration { get; set; }
+		public Action<IServiceCollection> DependencyInjection { get; set; }
+		public Action<IClientBuilder> Plugins { get; set; }
+	}
+}
