96bfb7365e7ea7977c47954a4909884b0baa0170
par.dahlman <par.dahlman@valtech.se>
2016-10-26 08:18:38 +0200

    (#131) Build StageMarkers to right place

--- src/RawRabbit.vNext/Pipe/PipeBuilder.cs
+++ src/RawRabbit.vNext/Pipe/PipeBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using RawRabbit.Pipe;
 using RawRabbit.Pipe.Middleware;
 
 namespace RawRabbit.vNext.Pipe
@@ -8,10 +10,35 @@ namespace RawRabbit.vNext.Pipe
 	public class PipeBuilder : RawRabbit.Pipe.PipeBuilder
 	{
 		private readonly IServiceProvider _provider;
+		private readonly Action<IPipeBuilder> _additional;
 
 		public PipeBuilder(IServiceProvider provider)
 		{
 			_provider = provider;
+			_additional = provider.GetService<Action<IPipeBuilder>>();
+		}
+
+		public override Middleware Build()
+		{
+			_additional(this);
+
+			var stageMarkerOptions = Pipe
+				.Where(info => info.Type == typeof(StageMarkerMiddleware))
+				.SelectMany(info => info.ConstructorArgs.OfType<StageMarkerOptions>());
+
+			var stageMwInfo = Pipe.Where(info => typeof(StagedMiddleware).IsAssignableFrom(info.Type));
+			var stagedMiddleware = stageMwInfo
+				.Select(CreateInstance)
+				.OfType<StagedMiddleware>()
+				.ToList();
+
+			foreach (var stageMarkerOption in stageMarkerOptions)
+			{
+				var thisStageMws = stagedMiddleware.Where(mw => mw.StageMarker == stageMarkerOption.StageMarker).ToList<Middleware>();
+				stageMarkerOption.EntryPoint = Build(thisStageMws);
+			}
+			Pipe = Pipe.Except(stageMwInfo).ToList();
+			return base.Build();
 		}
 
 		protected override Middleware CreateInstance(MiddlewareInfo middlewareInfo)
