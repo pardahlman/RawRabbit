2388f459d0321245fcecd30250426831d2cb3baf
par.dahlman <par.dahlman@valtech.se>
2016-10-26 08:19:20 +0200

    (#131) Use StageMarks for Publish

--- src/RawRabbit/Pipe/Client/MessagePublishExtension.cs
+++ src/RawRabbit/Pipe/Client/MessagePublishExtension.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Threading.Tasks;
 using RawRabbit.Configuration.Publish;
 using RawRabbit.Context;
@@ -8,29 +9,36 @@ namespace RawRabbit.Pipe.Client
 {
 	public static class MessagePublishExtension
 	{
+		private static readonly Action<IPipeBuilder> PublishPipeAction = pipe => pipe
+
+			.Use<OperationConfigurationMiddleware>()
+			.Use<MessageChainingMiddleware>()
+			.Use<GlobalMessageIdMiddleware>()
+			.Use<RoutingKeyMiddleware>()
+			.Use<ExchangeDeclareMiddleware>()
+			.Use<MessageContextCreationMiddleware<MessageContext>>()
+			.Use<ContextSerializationMiddleware>()
+			.Use<BasicPropertiesMiddleware>()
+			.Use<MessageSerializationMiddleware>()
+			.Use<StageMarkerMiddleware>(StageMarkerOptions.For(StageMarker.PreChannelCreation))
+			.Use<PublishChannelMiddleware>()
+			.Use<StageMarkerMiddleware>(StageMarkerOptions.For(StageMarker.PostChannelCreation))
+			.Use<PublishAcknowledgeMiddleware>()
+			.Use<StageMarkerMiddleware>(StageMarkerOptions.For(StageMarker.PreMessagePublish))
+			.Use<PublishMessage>()
+			.Use<StageMarkerMiddleware>(StageMarkerOptions.For(StageMarker.PostMessagePublish));
+
 		public static Task PublishAsync<TMessage>(this IBusClient client, TMessage message, Action<IPublishConfigurationBuilder> config = null)
 		{
-			return client.InvokeAsync(pipe => pipe
-				.Use((context, next) =>
+			return client.InvokeAsync(
+				PublishPipeAction,
+				ctx =>
 				{
-					context.Properties.Add(PipeKey.Operation, Operation.Publish);
-					context.Properties.Add(PipeKey.MessageType, typeof(TMessage));
-					context.Properties.Add(PipeKey.Message, message);
-					context.Properties.Add(PipeKey.ConfigurationAction, config);
-					return next();
-				})
-				.Use<OperationConfigurationMiddleware>()
-				.Use<MessageChainingMiddleware>()
-				.Use<GlobalMessageIdMiddleware>()
-				.Use<RoutingKeyMiddleware>()
-				.Use<ExchangeDeclareMiddleware>()
-				.Use<MessageContextCreationMiddleware<MessageContext>>()
-				.Use<ContextSerializationMiddleware>()
-				.Use<BasicPropertiesMiddleware>()
-				.Use<MessageSerializationMiddleware>()
-				.Use<PublishChannelMiddleware>()
-				.Use<PublishAcknowledgeMiddleware>()
-				.Use<PublishMessage>());
+					ctx.Properties.Add(PipeKey.Operation, Operation.Publish);
+					ctx.Properties.Add(PipeKey.MessageType, typeof(TMessage));
+					ctx.Properties.Add(PipeKey.Message, message);
+					ctx.Properties.Add(PipeKey.ConfigurationAction, config);
+				});
 		}
 	}
 }
