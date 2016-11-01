737647a51a9c9a70f031e28510ca01cf044e71ca
par.dahlman <par.dahlman@fellowdeveloper.se>
2016-11-01 23:05:37 +0100

    (#122) Add disposable Instance Factory

---
+++ src/RawRabbit/Instantiation/InstanceFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RawRabbit.Common;
+using RawRabbit.DependecyInjection;
+using RawRabbit.Pipe;
+
+namespace RawRabbit.Instantiation
+{
+	public class InstanceFactory : IDisposable
+	{
+		private readonly IDependecyResolver _resolver;
+
+		public InstanceFactory(IDependecyResolver resolver)
+		{
+			_resolver = resolver;
+		}
+
+		public IBusClient Create()
+		{
+			var pipeBuliderFactory = new PipeBuilderFactory(() => new PipeBuilder(_resolver));
+			return new BusClient(pipeBuliderFactory, _resolver.GetService<IPipeContextFactory>());
+		}
+
+		public void Dispose()
+		{
+			var diposer = _resolver.GetService<IResourceDisposer>();
+			diposer?.Dispose();
+		}
+	}
+}
