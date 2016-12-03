2ef0b3dfd6f4326cb2ada1aa0c11a80748c8fcaf
par.dahlman <par.dahlman@fellowdeveloper.se>
2016-12-03 08:20:26 +0100

    Add files ignored by git

---
+++ src/RawRabbit/Configuration/Legacy/Publish/IPublishConfigurationBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RawRabbit.Configuration.Exchange;
+
+namespace RawRabbit.Configuration.Legacy.Publish
+{
+	public interface IPublishConfigurationBuilder
+	{
+		IPublishConfigurationBuilder WithExchange(Action<IExchangeDeclarationBuilder> exchange);
+		IPublishConfigurationBuilder WithRoutingKey(string routingKey);
+		IPublishConfigurationBuilder WithProperties(Action<IBasicProperties> properties);
+		IPublishConfigurationBuilder WithMandatoryDelivery(EventHandler<BasicReturnEventArgs> basicReturn);
+	}
+}
---
+++ src/RawRabbit/Configuration/Legacy/Publish/PublishConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RawRabbit.Configuration.Exchange;
+
+namespace RawRabbit.Configuration.Legacy.Publish
+{
+	public class PublishConfiguration
+	{
+		public ExchangeDeclaration Exchange { get; set; }
+		public string RoutingKey { get; set; }
+		public Action<IBasicProperties> PropertyModifier { get; set; }
+		public EventHandler<BasicReturnEventArgs> BasicReturn { get; set; }
+	}
+}
