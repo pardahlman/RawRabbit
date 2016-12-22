fe46f8992426962dfcf2b0957588d814190c6a04
par.dahlman <par.dahlman@fellowdeveloper.se>
2016-12-22 16:28:39 +0100

    (#119) Verify concurrency for Rpc

--- test/RawRabbit.IntegrationTests/PublishAndSubscribe/MultipleOperationsTests.cs
+++ test/RawRabbit.IntegrationTests/PublishAndSubscribe/MultipleOperationsTests.cs
@@ -39,5 +39,30 @@ namespace RawRabbit.IntegrationTests.PublishAndSubscribe
 				Assert.Equal(recievedTcs.Task.Result, sendCount);
 			}
 		}
+
+		[Fact]
+		public async Task Should_Be_Able_To_Perform_Multiple_Concurrent_Rpc()
+		{
+			using (var requester = RawRabbitFactory.CreateTestClient())
+			using (var responder = RawRabbitFactory.CreateTestClient())
+			{
+				/* Setup */
+				const int sendCount = 2000;
+				var publishTasks = new Task[sendCount];
+				await responder.RespondAsync<BasicRequest, BasicResponse>(recieved =>
+					Task.FromResult(new BasicResponse())
+				);
+
+				/* Test */
+				for (var i = 0; i < sendCount; i++)
+				{
+					publishTasks[i] = requester.RequestAsync<BasicRequest, BasicResponse>();
+				}
+				Task.WaitAll(publishTasks);
+
+				/* Assert */
+				Assert.True(true);
+			}
+		}
 	}
 }
