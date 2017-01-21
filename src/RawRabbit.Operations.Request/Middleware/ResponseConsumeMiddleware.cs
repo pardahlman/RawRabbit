e61f2cdd38af55fcce7bae030dbbf193d4fed1ff
par.dahlman <par.dahlman@fellowdeveloper.se>
2017-01-21 22:28:40 +0100

    (#123) Address performance for Response consume

--- src/RawRabbit.Operations.Request/Middleware/ResponseConsumeMiddleware.cs
+++ src/RawRabbit.Operations.Request/Middleware/ResponseConsumeMiddleware.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RawRabbit.Configuration.Consumer;
 using RawRabbit.Consumer;
 using RawRabbit.Logging;
 using RawRabbit.Operations.Request.Core;
@@ -11,59 +15,74 @@ namespace RawRabbit.Operations.Request.Middleware
 	public class ResponseConsumerOptions
 	{
 		public Action<IPipeBuilder> ResponseRecieved { get; set; }
+		public Func<IPipeContext, ConsumerConfiguration> ResponseConfigFunc { get; set; }
+		public Func<IPipeContext, string> CorrelationIdFunc { get; set; }
 	}
 
 	public class ResponseConsumeMiddleware : Pipe.Middleware.Middleware
 	{
-		private readonly IConsumerFactory _consumerFactory;
-		private readonly Pipe.Middleware.Middleware _responsePipe;
+		protected readonly IConsumerFactory ConsumerFactory;
+		protected readonly Pipe.Middleware.Middleware ResponsePipe;
 		private readonly ILogger _logger = LogManager.GetLogger<ResponseConsumeMiddleware>();
+		protected readonly ConcurrentDictionary<IBasicConsumer, ConcurrentDictionary<string, TaskCompletionSource<BasicDeliverEventArgs>>> AllResponses;
+		protected Func<IPipeContext, ConsumerConfiguration> ResponseConfigFunc;
+		protected Func<IPipeContext, string> CorrelationidFunc;
 
 		public ResponseConsumeMiddleware(IConsumerFactory consumerFactory, IPipeBuilderFactory factory, ResponseConsumerOptions options)
 		{
-			_consumerFactory = consumerFactory;
-			_responsePipe = factory.Create(options.ResponseRecieved);
+			ResponseConfigFunc = options?.ResponseConfigFunc ?? (context => context.GetResponseConfiguration());
+			CorrelationidFunc = options?.CorrelationIdFunc ?? (context => context.GetBasicProperties()?.CorrelationId);
+			ConsumerFactory = consumerFactory;
+			ResponsePipe = factory.Create(options.ResponseRecieved);
+			AllResponses = new ConcurrentDictionary<IBasicConsumer, ConcurrentDictionary<string, TaskCompletionSource<BasicDeliverEventArgs>>>();
 		}
 
-		public override Task InvokeAsync(IPipeContext context, CancellationToken token)
+		public override async Task InvokeAsync(IPipeContext context, CancellationToken token)
 		{
-			var respondCfg = context.GetResponseConfiguration();
-			var correlationId = context.GetBasicProperties()?.CorrelationId;
+			var respondCfg = GetResponseConfig(context);
+			var correlationId = GetCorrelationid(context);
+			var responseTsc = new TaskCompletionSource<BasicDeliverEventArgs>();
 
-			var executionTsc = new TaskCompletionSource<bool>();
-
-			_consumerFactory
-				.GetConsumerAsync(respondCfg.Consume, token: token)
-				.ContinueWith(tConsumer =>
+			var consumer = await ConsumerFactory.GetConsumerAsync(respondCfg.Consume, token: token);
+			var responses = AllResponses.GetOrAdd(consumer, c =>
 				{
-					context.Properties.Add(PipeKey.Consumer, tConsumer.Result);
-					tConsumer.Result.OnMessage((sender, args) =>
+					var pendings = new ConcurrentDictionary<string, TaskCompletionSource<BasicDeliverEventArgs>>();
+					c.OnMessage((sender, args) =>
 					{
-						if (!string.Equals(args.BasicProperties.CorrelationId, correlationId))
-						{
+						TaskCompletionSource<BasicDeliverEventArgs> tsc;
+						if (!pendings.TryGetValue(args.BasicProperties.CorrelationId, out tsc))
 							return;
-						}
-						_logger.LogInformation($"Message '{args.BasicProperties.MessageId}' for correlatrion '{correlationId}' recieved.");
-						context.Properties.Add(PipeKey.DeliveryEventArgs, args);
-						_responsePipe
-							.InvokeAsync(context, token)
-							.ContinueWith(t =>
-							{
-								if (t.IsFaulted)
-								{
-									_logger.LogError($"Response pipe for message '{args.BasicProperties.MessageId}' executed unsuccessfully.", t.Exception);
-									executionTsc.TrySetException(t.Exception?.InnerException);
-								}
-								else
-								{
-									executionTsc.TrySetResult(t.IsCompleted);
-								}
-							}, token);
-					}, abort: args => string.Equals(args.BasicProperties.CorrelationId, correlationId));
-					Next.InvokeAsync(context, token);
-				}, token);
+						tsc.TrySetResult(args);
+					});
+					return pendings;
+				}
+			);
 
-			return executionTsc.Task;
+			context.Properties.Add(PipeKey.Consumer, consumer);
+			responses.TryAdd(correlationId, responseTsc);
+			await Next.InvokeAsync(context, token);
+			await responseTsc.Task;
+			_logger.LogInformation($"Message '{responseTsc.Task.Result.BasicProperties.MessageId}' for correlatrion '{correlationId}' recieved.");
+			context.Properties.Add(PipeKey.DeliveryEventArgs, responseTsc.Task.Result);
+			try
+			{
+				await ResponsePipe.InvokeAsync(context, token);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError($"Response pipe for message '{responseTsc.Task.Result.BasicProperties.MessageId}' executed unsuccessfully.", e);
+				throw;
+			}
+		}
+
+		protected virtual ConsumerConfiguration GetResponseConfig(IPipeContext context)
+		{
+			return ResponseConfigFunc?.Invoke(context);
+		}
+
+		protected virtual string GetCorrelationid(IPipeContext context)
+		{
+			return CorrelationidFunc?.Invoke(context);
 		}
 	}
 }
