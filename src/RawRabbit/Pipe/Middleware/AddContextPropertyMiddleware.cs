d8c4787b5739ff7aca635348fff0c1f3b9f7739e
par.dahlman <par.dahlman@fellowdeveloper.se>
2017-01-29 17:28:48 +0100

    (#123) Add Override option for AddContextProp

--- src/RawRabbit/Pipe/Middleware/AddContextPropertyMiddleware.cs
+++ src/RawRabbit/Pipe/Middleware/AddContextPropertyMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using RawRabbit.Logging;
 
 namespace RawRabbit.Pipe.Middleware
 {
@@ -8,17 +9,21 @@ namespace RawRabbit.Pipe.Middleware
 	{
 		public Func<IPipeContext, string> KeyFunc { get; set; }
 		public Func<IPipeContext, object> ValueFunc { get; set; }
+		public Func<IPipeContext, bool> OverrideExistingFunc { get; set; }
 	}
 
 	public class AddContextPropertyMiddleware : Middleware
 	{
 		protected Func<IPipeContext, string> KeyFunc;
 		protected Func<IPipeContext, object> ValueFunc;
+		protected Func<IPipeContext, bool> OverrideExistingFunc;
+		private readonly ILogger _logger = LogManager.GetLogger<AddContextPropertyMiddleware>();
 
-		public AddContextPropertyMiddleware(AddContextPropertyOptions options = null)
+		public AddContextPropertyMiddleware(AddContextPropertyOptions options)
 		{
 			KeyFunc = options?.KeyFunc;
 			ValueFunc = options?.ValueFunc;
+			OverrideExistingFunc = options?.OverrideExistingFunc ?? (context => true);
 		}
 
 		public override Task InvokeAsync(IPipeContext context, CancellationToken token)
@@ -26,25 +31,50 @@ namespace RawRabbit.Pipe.Middleware
 			var key = GetKey(context);
 			if (string.IsNullOrEmpty(key))
 			{
+				_logger.LogInformation("Key not found");
 				return Next.InvokeAsync(context, token);
 			}
+			_logger.LogDebug($"Preparing to append '{key}' to Execution Context.");
 			var value = GetValue(context);
 			if (value == null)
 			{
+				_logger.LogInformation($"Value not found for key '{key}'");
 				return Next.InvokeAsync(context, token);
 			}
-			context.Properties.TryAdd(key, value);
+			if (!context.Properties.ContainsKey(key))
+			{
+				_logger.LogDebug("Context does not contain {key}.");
+				context.Properties.Add(key, value);
+				return Next.InvokeAsync(context, token);
+			}
+			var overrideExisting = OverrideExisting(context);
+			if (overrideExisting)
+			{
+				_logger.LogInformation($"Context contains key {key}. Removing key.");
+				context.Properties[key] = value;
+			}
+
 			return Next.InvokeAsync(context, token);
+		}
+
+		protected virtual bool OverrideExisting(IPipeContext context)
+		{
+			if (OverrideExistingFunc == null)
+			{
+				_logger.LogInformation("Override Existing Func not set. Defaulting to false.");
+				return false;
+			}
+			return OverrideExistingFunc.Invoke(context);
 		}
 
 		protected virtual string GetKey(IPipeContext context)
 		{
-			return KeyFunc(context);
+			return KeyFunc?.Invoke(context);
 		}
 
 		protected virtual object GetValue(IPipeContext context)
 		{
-			return ValueFunc(context);
+			return ValueFunc?.Invoke(context);
 		}
 	}
 }
