0d2b99eb7282c347f846595988154491c1dc211d
par.dahlman <par.dahlman@fellowdeveloper.se>
2017-02-19 09:31:47 +0100

    (#189) Add LogContext Middleware + Plugin

---
+++ src/RawRabbit.Logging.Serilog/LogContextMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RawRabbit.Pipe;
+using RawRabbit.Pipe.Middleware;
+using Serilog.Context;
+using Serilog.Core;
+
+namespace RawRabbit.Logging.Serilog
+{
+	public class LogContextOptions
+	{
+		public Func<IPipeContext, ILogEventEnricher[]> LogEnricherFunc { get; set; }
+	}
+
+	public class LogContextMiddleware : StagedMiddleware
+	{
+		protected Func<IPipeContext, ILogEventEnricher[]> LogEnricherFunc;
+		public override string StageMarker => Pipe.StageMarker.MessageDeserialized;
+
+		public LogContextMiddleware(LogContextOptions options = null)
+		{
+			LogEnricherFunc = context => context.GetLogEventEnrichers() ?? options?.LogEnricherFunc?.Invoke(context);
+		}
+
+		public override async Task InvokeAsync(IPipeContext context, CancellationToken token = new CancellationToken())
+		{
+			var enrichers = GetLogEventEnrichers(context);
+			using (LogContext.PushProperties(enrichers))
+			{
+				await Next.InvokeAsync(context, token);
+			}
+		}
+
+		protected virtual ILogEventEnricher[] GetLogEventEnrichers(IPipeContext context)
+		{
+			return LogEnricherFunc?.Invoke(context);
+		}
+	}
+
+	public static class LogContextPipeExtensions
+	{
+		private const string LogEventEnrichers = "Serilog:LogEventEnrichers";
+
+		public static ILogEventEnricher[] GetLogEventEnrichers(this IPipeContext context)
+		{
+			return context.Get<ILogEventEnricher[]>(LogEventEnrichers);
+		}
+
+		public static IPipeContext UseLogEventEnrichers(this IPipeContext context, params ILogEventEnricher[] enrichers)
+		{
+			context.Properties.AddOrReplace(LogEventEnrichers, enrichers.ToArray());
+			return context;
+		}
+	}
+}
---
+++ src/RawRabbit.Logging.Serilog/LogContextPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using RawRabbit.Instantiation;
+using RawRabbit.Logging.Serilog;
+using RawRabbit.Pipe;
+using Serilog.Core;
+
+namespace RawRabbit
+{
+	public static class LogContextPlugin
+	{
+		public static IClientBuilder UseSerilogEnrichers(this IClientBuilder builder, Func<IPipeContext, ILogEventEnricher[]> enrichers)
+		{
+			builder.Register(pipe => pipe
+				.Use<LogContextMiddleware>(new LogContextOptions
+				{
+					LogEnricherFunc =  enrichers
+				}));
+			return builder;
+		}
+	}
+}
