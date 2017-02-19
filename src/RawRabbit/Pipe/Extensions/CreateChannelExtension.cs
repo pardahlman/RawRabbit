72c732019654408a470e112620277f2bbf4a7e9a
par.dahlman <par.dahlman@fellowdeveloper.se>
2017-02-19 14:55:41 +0100

    (#123) Add advanced helper methods

--- src/RawRabbit/Pipe/Extensions/CreateChannelExtension.cs
+++ src/RawRabbit/Pipe/Extensions/CreateChannelExtension.cs
@@ -2,9 +2,10 @@
 using System.Threading;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
+using RawRabbit.Pipe;
 using RawRabbit.Pipe.Middleware;
 
-namespace RawRabbit.Pipe.Extensions
+namespace RawRabbit.Advanced
 {
 	public static class CreateChannelExtension
 	{
