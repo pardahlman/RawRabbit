3a605b8bda35b40bdda81a7445f111f9ea9de675
par.dahlman <par.dahlman@fellowdeveloper.se>
2017-02-24 21:33:35 +0100

    (#195) Add extension for Queue/Exchange delete

---
+++ src/RawRabbit/Pipe/Extensions/DeleteQueueExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using RawRabbit.Configuration.Consumer;
+using RawRabbit.Pipe.Middleware;
+
+namespace RawRabbit.Pipe.Extensions
+{
+	public static class DeleteQueueExtension
+	{
+		private const string QueueName = "DeleteQueue:QueueName";
+
+		public static readonly Action<IPipeBuilder> DeletePipe = builder => builder
+			.Use<ConsumeConfigurationMiddleware>()
+			.Use((context, func) =>
+			{
+				var consumeCfg = context.GetConsumeConfiguration();
+				if (consumeCfg != null)
+				{
+					context.Properties.TryAdd(QueueName, consumeCfg.QueueName);
+				}
+				return func();
+			})
+			.Use<TransientChannelMiddleware>()
+			.Use<QueueDeleteMiddleware>(new QueueDeleteOptions
+			{
+				QueueNameFunc = context => context.Get<string>(QueueName)
+			});
+
+		public static Task DeleteQueueAsync(this IBusClient client, string queueName)
+		{
+			return client.InvokeAsync(DeletePipe, ctx => ctx.Properties.Add(QueueName, queueName));
+		}
+
+		public static Task DeleteQueueAsync<TMessage>(this IBusClient client)
+		{
+			return client.InvokeAsync(DeletePipe, ctx => ctx.Properties.Add(PipeKey.MessageType, typeof(TMessage)));
+		}
+	}
+}
---
+++ src/RawRabbit/Pipe/Middleware/ExchangeDeleteMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class ExchangeDeleteOptions
+	{
+		public Func<IPipeContext, IModel> ChannelFunc { get; set; }
+		public Func<IPipeContext, string> ExchangeNameFunc { get; set; }
+		public Func<IPipeContext, bool> IfUsedFunc { get; set; }
+	}
+
+	public class ExchangeDeleteMiddleware : Middleware
+	{
+		protected Func<IPipeContext, IModel> ChannelFunc;
+		protected Func<IPipeContext, string> ExchangeNameFunc;
+		protected Func<IPipeContext, bool> IfUsedFunc;
+
+		public ExchangeDeleteMiddleware(ExchangeDeleteOptions options)
+		{
+			ChannelFunc = options?.ChannelFunc ?? (context => context.GetTransientChannel());
+			ExchangeNameFunc = options?.ExchangeNameFunc ?? (context => string.Empty);
+			IfUsedFunc = options?.IfUsedFunc ?? (context => false);
+		}
+
+		public override Task InvokeAsync(IPipeContext context, CancellationToken token = new CancellationToken())
+		{
+			var channel = GetChannel(context);
+			var exchangeName = GetExchangeName(context);
+			var ifUsed = GetIfUsed(context);
+			DeleteEchange(channel, exchangeName, ifUsed);
+			return Next.InvokeAsync(context, token);
+		}
+
+		protected virtual void DeleteEchange(IModel channel, string exchangeName, bool ifUsed)
+		{
+			channel.ExchangeDelete(exchangeName, ifUsed);
+		}
+
+		protected virtual IModel GetChannel(IPipeContext context)
+		{
+			return ChannelFunc?.Invoke(context);
+		}
+
+		protected virtual string GetExchangeName(IPipeContext context)
+		{
+			return ExchangeNameFunc?.Invoke(context);
+		}
+
+		protected virtual bool GetIfUsed(IPipeContext context)
+		{
+			return IfUsedFunc.Invoke(context);
+		}
+	}
+}
