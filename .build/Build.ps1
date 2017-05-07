abfbd85672ba891aa7618142ea750c26bad8dd6d
pardahlman <par.dahlman@fellowdeveloper.se>
2017-05-07 20:46:03 +0200

    (#211) Add temporary work around for version prefix

--- .build/Build.ps1
+++ .build/Build.ps1
@@ -20,7 +20,8 @@ foreach ($src in ls ../src/*) {
 
 	write-host "build: Packaging project in $src" -ForegroundColor Green
 
-	& dotnet pack -c Release -o ..\..\artifacts --version-suffix="beta2"
+	& dotnet msbuild "/t:Restore;Pack" /p:VersionSuffix=beta6 /p:Configuration=Release
+	# & dotnet pack -c Release -o ..\..\artifacts --version-suffix="beta2"
 	if($LASTEXITCODE -ne 0) { exit 1 }
 
 	Pop-Location
