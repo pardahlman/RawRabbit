c66be0c13eef2877778f5b2c0d9ada8aeb9893e6
pardahlman <par.dahlman@fellowdeveloper.se>
2017-08-19 23:36:16 +0200

    (#255) Add Protobug serializer

---
+++ RawRabbit.Enrichers.Protobuf/ProtobufPlugin.cs
@@ -0,0 +1,22 @@
+﻿using RawRabbit.Enrichers.Protobuf;
+using RawRabbit.Instantiation;
+using RawRabbit.Serialization;
+
+namespace RawRabbit
+{
+	public static class ProtobufPlugin
+	{
+		/// <summary>
+		/// Replaces the default serializer with Protobuf.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <returns></returns>
+		public static IClientBuilder UseProtobuf(this IClientBuilder builder)
+		{
+			builder.Register(
+				pipe: p => {},
+				ioc: di => di.AddSingleton<ISerializer, ProtobufSerializer>());
+			return builder;
+		}
+	}
+}
---
+++ RawRabbit.Enrichers.Protobuf/ProtobufSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+using RawRabbit.Serialization;
+
+namespace RawRabbit.Enrichers.Protobuf
+{
+	public class ProtobufSerializer : ISerializer
+	{
+		public string ContentType => "application/x-protobuf";
+
+		public byte[] Serialize(object obj)
+		{
+			using (var memoryStream = new MemoryStream())
+			{
+				Serializer.Serialize(memoryStream, obj);
+				return memoryStream.ToArray();
+			}
+		}
+
+		public object Deserialize(Type type, byte[] bytes)
+		{
+			using (var memoryStream = new MemoryStream(bytes))
+			{
+				return Serializer.Deserialize(type, memoryStream);
+			}
+		}
+
+		public TType Deserialize<TType>(byte[] bytes)
+		{
+			using (var memoryStream = new MemoryStream(bytes))
+			{
+				return Serializer.Deserialize<TType>(memoryStream);
+			}
+		}
+	}
+}
