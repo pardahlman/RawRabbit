f4d54f2a2819a415c570951d7413e823d01fa7b4
pardahlman <par.dahlman@fellowdeveloper.se>
2017-10-08 15:33:34 +0200

    (#279) Add ChannelPoolFactory and middleware
    These classes together makes it possible to easy
    create a unique channel pool in the operations
    pipes without creating any derived class.

---
+++ src/RawRabbit/Channel/AutoScalingChannelPoolFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Concurrent;
+using RawRabbit.Channel.Abstraction;
+
+namespace RawRabbit.Channel
+{
+	public interface IChannelPoolFactory
+	{
+		IChannelPool GetChannelPool(string name = null);
+	}
+
+	public class AutoScalingChannelPoolFactory : IChannelPoolFactory, IDisposable
+	{
+		private readonly IChannelFactory _factory;
+		private readonly AutoScalingOptions _options;
+		private readonly ConcurrentDictionary<string, Lazy<IChannelPool>> _channelPools;
+		private const string DefaultPoolName = "default";
+
+		public AutoScalingChannelPoolFactory(IChannelFactory factory, AutoScalingOptions options = null)
+		{
+			_factory = factory;
+			_options = options ?? AutoScalingOptions.Default;
+			_channelPools = new ConcurrentDictionary<string, Lazy<IChannelPool>>();
+		}
+
+		public IChannelPool GetChannelPool(string name = null)
+		{
+			name = name ?? DefaultPoolName;
+			var pool = _channelPools.GetOrAdd(name, s => new Lazy<IChannelPool>(() => new AutoScalingChannelPool(_factory, _options)));
+			return pool.Value;
+		}
+
+		public void Dispose()
+		{
+			_factory?.Dispose();
+			foreach (var channelPool in _channelPools.Values)
+			{
+				(channelPool.Value as IDisposable)?.Dispose();
+			}
+		}
+	}
+}
---
+++ src/RawRabbit/Pipe/Middleware/PooledChannelMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RawRabbit.Channel;
+
+namespace RawRabbit.Pipe.Middleware
+{
+	public class PooledChannelOptions
+	{
+		public Func<IPipeContext, string> PoolNameFunc { get; set; }
+		public Action<IPipeContext, IModel> SaveInContextAction { get; set; }
+	}
+
+	public class PooledChannelMiddleware : Middleware
+	{
+		protected readonly IChannelPoolFactory PoolFactory;
+		protected readonly Func<IPipeContext, string> PoolNameFunc;
+		protected readonly Action<IPipeContext, IModel> SaveInContextAction;
+
+		public PooledChannelMiddleware(IChannelPoolFactory poolFactory, PooledChannelOptions options = null)
+		{
+			PoolFactory = poolFactory;
+			PoolNameFunc = options?.PoolNameFunc;
+			SaveInContextAction = options?.SaveInContextAction ?? ((ctx, value) =>ctx.Properties.TryAdd(PipeKey.TransientChannel, value));
+		}
+
+		public override async Task InvokeAsync(IPipeContext context, CancellationToken token = default(CancellationToken))
+		{
+			var channel = await GetChannelAsync(context, token);
+			SaveInContext(context, channel);
+			await Next.InvokeAsync(context, token);
+		}
+
+		protected virtual string GetChannelPoolName(IPipeContext context)
+		{
+			return PoolNameFunc?.Invoke(context);
+		}
+
+		protected virtual IChannelPool GetChannelPool(IPipeContext context)
+		{
+			var poolName = GetChannelPoolName(context);
+			return PoolFactory.GetChannelPool(poolName);
+		}
+
+		protected virtual Task<IModel> GetChannelAsync(IPipeContext context, CancellationToken ct)
+		{
+			var channelPool = GetChannelPool(context);
+			return channelPool.GetAsync(ct);
+		}
+
+		protected virtual void SaveInContext(IPipeContext context, IModel channel)
+		{
+			SaveInContextAction?.Invoke(context, channel);
+		}
+	}
+}
