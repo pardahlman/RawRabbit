f8c091f2faa93ba9e1ae5afc72835b15f16fe741
pardahlman <par.dahlman@fellowdeveloper.se>
2017-10-08 15:49:50 +0200

    (#279) Add Polly test project

---
+++ test/RawRabbit.Enrichers.Polly.Tests/Middleware/QueueDeclareMiddlewareTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using RawRabbit.Common;
+using RawRabbit.Configuration.Queue;
+using RawRabbit.Pipe;
+using RawRabbit.Pipe.Middleware;
+using Xunit;
+using QueueDeclareMiddleware = RawRabbit.Enrichers.Polly.Middleware.QueueDeclareMiddleware;
+
+namespace RawRabbit.Enrichers.Polly.Tests.Middleware
+{
+	public class QueueDeclareMiddlewareTests
+	{
+		[Fact]
+		public async Task Should_Invoke_Queue_Declare_Policy_With_Correct_Context()
+		{
+			var topology = new Mock<ITopologyProvider>();
+			var queueDeclaration = new QueueDeclaration();
+			var policyCalled = false;
+			Context capturedContext = null;
+
+			topology
+				.SetupSequence(t => t.DeclareQueueAsync(queueDeclaration))
+				.Throws(new OperationInterruptedException(null))
+				.Returns(Task.CompletedTask);
+
+			var context = new PipeContext
+			{
+				Properties = new Dictionary<string, object>
+				{
+					{PipeKey.QueueDeclaration, queueDeclaration}
+				}
+			};
+
+			context.UsePolicy(Policy
+				.Handle<OperationInterruptedException>()
+				.RetryAsync((exception, retryCount, pollyContext) =>
+				{
+					policyCalled = true;
+					capturedContext = pollyContext;
+				}), PolicyKeys.QueueDeclare);
+			var middleware = new QueueDeclareMiddleware(topology.Object) {Next = new NoOpMiddleware()};
+
+			/* Test */
+			await middleware.InvokeAsync(context);
+
+			/* Assert */
+			Assert.True(policyCalled, "Should call policy");
+			Assert.Equal(context, capturedContext[RetryKey.PipeContext]);
+			Assert.Equal(queueDeclaration, capturedContext[RetryKey.QueueDeclaration]);
+			Assert.Equal(topology.Object, capturedContext[RetryKey.TopologyProvider]);
+		}
+	}
+}
---
+++ test/RawRabbit.Enrichers.Polly.Tests/Services/ChannelFactoryTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using RawRabbit.Configuration;
+using RawRabbit.Enrichers.Polly.Services;
+using Xunit;
+
+namespace RawRabbit.Enrichers.Polly.Tests.Services
+{
+	public class ChannelFactoryTests
+	{
+		[Fact]
+		public async Task Should_Use_Connect_Policy_When_Connecting_To_Broker()
+		{
+			/* Setup */
+			var connection = new Mock<IConnection>();
+			var connectionFactory = new Mock<IConnectionFactory>();
+			connectionFactory
+				.Setup(f => f.CreateConnection())
+				.Returns(connection.Object);
+			connectionFactory
+				.SetupSequence(c => c.CreateConnection(
+						It.IsAny<List<string>>()
+					))
+				.Throws(new BrokerUnreachableException(new Exception()))
+				.Throws(new BrokerUnreachableException(new Exception()))
+				.Throws(new BrokerUnreachableException(new Exception()))
+				.Returns(connection.Object);
+
+			var policy = Policy
+				.Handle<BrokerUnreachableException>()
+				.WaitAndRetryAsync(new[]
+				{
+					TimeSpan.FromMilliseconds(1),
+					TimeSpan.FromMilliseconds(2),
+					TimeSpan.FromMilliseconds(4),
+					TimeSpan.FromMilliseconds(8),
+					TimeSpan.FromMilliseconds(16)
+				});
+
+			var factory = new ChannelFactory(connectionFactory.Object, RawRabbitConfiguration.Local, new ConnectionPolicies{ Connect = policy});
+
+			/* Test */
+			/* Assert */
+			await factory.ConnectAsync();
+		}
+
+		[Fact]
+		public async Task Should_Use_Create_Channel_Policy_When_Creaing_Channels()
+		{
+			/* Setup */
+			var channel = new Mock<IModel>();
+			var connection = new Mock<IConnection>();
+			var connectionFactory = new Mock<IConnectionFactory>();
+			connectionFactory
+				.Setup(f => f.CreateConnection())
+				.Returns(connection.Object);
+			connectionFactory
+				.Setup(c => c.CreateConnection(
+					It.IsAny<List<string>>()
+				))
+				.Returns(connection.Object);
+			connection
+				.Setup(c => c.IsOpen)
+				.Returns(true);
+			connection
+				.SetupSequence(c => c.CreateModel())
+				.Throws(new TimeoutException())
+				.Throws(new TimeoutException())
+				.Returns(channel.Object);
+
+			var policy = Policy
+				.Handle<TimeoutException>()
+				.WaitAndRetryAsync(new[]
+				{
+					TimeSpan.FromMilliseconds(1),
+					TimeSpan.FromMilliseconds(2),
+					TimeSpan.FromMilliseconds(4),
+					TimeSpan.FromMilliseconds(8),
+					TimeSpan.FromMilliseconds(16)
+				});
+
+			var factory = new ChannelFactory(connectionFactory.Object, RawRabbitConfiguration.Local, new ConnectionPolicies { CreateChannel = policy });
+
+			/* Test */
+			var retrievedChannel = await  factory.CreateChannelAsync();
+
+			/* Assert */
+			Assert.Equal(channel.Object, retrievedChannel);
+		}
+	}
+}
