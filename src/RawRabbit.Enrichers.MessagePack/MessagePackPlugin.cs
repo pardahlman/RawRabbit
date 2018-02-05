4ea5cff6b570b0ceb5e4fdba0fdf15f2dac5b1da
Michael Bisbjerg <michael@mbwarez.dk>
2018-02-05 22:16:10 +0100

    Add initial version of MessagePack formatter

---
+++ src/RawRabbit.Enrichers.MessagePack/MessagePackPlugin.cs
@@ -0,0 +1,19 @@
+﻿using RawRabbit.Instantiation;
+using RawRabbit.Serialization;
+
+namespace RawRabbit.Enrichers.MessagePack
+{
+    public static class MessagePackPlugin
+	{
+		/// <summary>
+		/// Replaces the default serializer with MessagePack.
+		/// </summary>
+		public static IClientBuilder UseMessagePack(this IClientBuilder builder)
+        {
+            builder.Register(
+                pipe: p => { },
+                ioc: di => di.AddSingleton<ISerializer, MessagePackSerializerWorker>());
+            return builder;
+        }
+    }
+}
---
+++ src/RawRabbit.Enrichers.MessagePack/MessagePackSerializerWorker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using MessagePack;
+using RawRabbit.Serialization;
+
+namespace RawRabbit.Enrichers.MessagePack
+{
+    public class MessagePackSerializerWorker : ISerializer
+    {
+        public string ContentType => "application/x-messagepack";
+        private readonly MethodInfo _deserializeType;
+        private readonly MethodInfo _serializeType;
+
+        public MessagePackSerializerWorker()
+        {
+			_deserializeType = typeof(MessagePackSerializer)
+				.GetMethod(nameof(MessagePackSerializer.Deserialize), new[] { typeof(byte[]) });
+			_serializeType = typeof(MessagePackSerializer)
+				.GetMethods()
+				.FirstOrDefault(s => s.Name == nameof(MessagePackSerializer.Serialize) && s.ReturnType == typeof(byte[]));
+		}
+
+        public byte[] Serialize(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException();
+
+            return (byte[])_serializeType
+                .MakeGenericMethod(obj.GetType())
+                .Invoke(null, new[] { obj });
+        }
+
+        public object Deserialize(Type type, byte[] bytes)
+        {
+            return _deserializeType.MakeGenericMethod(type)
+                .Invoke(null, new object[] { bytes });
+        }
+
+        public TType Deserialize<TType>(byte[] bytes)
+        {
+            return MessagePackSerializer.Deserialize<TType>(bytes);
+        }
+    }
+}
