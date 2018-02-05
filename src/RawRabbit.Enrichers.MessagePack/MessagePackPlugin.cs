bde559ac11b6c893b749311a7b153ce501e04d3b
Michael Bisbjerg <michael@mbwarez.dk>
2018-02-05 23:04:56 +0100

    Add option to use LZ4
    - make the actual implementation internal

--- src/RawRabbit.Enrichers.MessagePack/MessagePackPlugin.cs
+++ src/RawRabbit.Enrichers.MessagePack/MessagePackPlugin.cs
@@ -3,17 +3,17 @@ using RawRabbit.Serialization;
 
 namespace RawRabbit.Enrichers.MessagePack
 {
-    public static class MessagePackPlugin
+	public static class MessagePackPlugin
 	{
 		/// <summary>
 		/// Replaces the default serializer with MessagePack.
 		/// </summary>
-		public static IClientBuilder UseMessagePack(this IClientBuilder builder)
-        {
-            builder.Register(
-                pipe: p => { },
-                ioc: di => di.AddSingleton<ISerializer, MessagePackSerializerWorker>());
-            return builder;
-        }
-    }
+		public static IClientBuilder UseMessagePack(this IClientBuilder builder, bool useLz4 = false)
+		{
+			builder.Register(
+				pipe: p => { },
+				ioc: di => di.AddSingleton<ISerializer, MessagePackSerializerWorker>(resolver => new MessagePackSerializerWorker(useLz4)));
+			return builder;
+		}
+	}
 }
--- src/RawRabbit.Enrichers.MessagePack/MessagePackSerializerWorker.cs
+++ src/RawRabbit.Enrichers.MessagePack/MessagePackSerializerWorker.cs
@@ -6,40 +6,47 @@ using RawRabbit.Serialization;
 
 namespace RawRabbit.Enrichers.MessagePack
 {
-    public class MessagePackSerializerWorker : ISerializer
-    {
-        public string ContentType => "application/x-messagepack";
-        private readonly MethodInfo _deserializeType;
-        private readonly MethodInfo _serializeType;
+	internal class MessagePackSerializerWorker : ISerializer
+	{
+		public string ContentType => "application/x-messagepack";
+		private readonly MethodInfo _deserializeType;
+		private readonly MethodInfo _serializeType;
 
-        public MessagePackSerializerWorker()
-        {
-			_deserializeType = typeof(MessagePackSerializer)
+		public MessagePackSerializerWorker(bool useLz4)
+		{
+			Type tp;
+
+			if (useLz4)
+				tp = typeof(LZ4MessagePackSerializer);
+			else
+				tp = typeof(MessagePackSerializer);
+
+			_deserializeType = tp
 				.GetMethod(nameof(MessagePackSerializer.Deserialize), new[] { typeof(byte[]) });
-			_serializeType = typeof(MessagePackSerializer)
+			_serializeType = tp
 				.GetMethods()
 				.FirstOrDefault(s => s.Name == nameof(MessagePackSerializer.Serialize) && s.ReturnType == typeof(byte[]));
 		}
 
-        public byte[] Serialize(object obj)
-        {
-            if (obj == null)
-                throw new ArgumentNullException();
+		public byte[] Serialize(object obj)
+		{
+			if (obj == null)
+				throw new ArgumentNullException();
 
-            return (byte[])_serializeType
-                .MakeGenericMethod(obj.GetType())
-                .Invoke(null, new[] { obj });
-        }
+			return (byte[])_serializeType
+				.MakeGenericMethod(obj.GetType())
+				.Invoke(null, new[] { obj });
+		}
 
-        public object Deserialize(Type type, byte[] bytes)
-        {
-            return _deserializeType.MakeGenericMethod(type)
-                .Invoke(null, new object[] { bytes });
-        }
+		public object Deserialize(Type type, byte[] bytes)
+		{
+			return _deserializeType.MakeGenericMethod(type)
+				.Invoke(null, new object[] { bytes });
+		}
 
-        public TType Deserialize<TType>(byte[] bytes)
-        {
-            return MessagePackSerializer.Deserialize<TType>(bytes);
-        }
-    }
+		public TType Deserialize<TType>(byte[] bytes)
+		{
+			return MessagePackSerializer.Deserialize<TType>(bytes);
+		}
+	}
 }
