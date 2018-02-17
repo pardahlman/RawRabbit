57e21aef2c5f6005cc00b9dcf69eee0515938b97
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2018-02-17 17:12:33 +0100

    (#288) Dispose Channel Pool Repository from ResourceDisposer

--- src/RawRabbit/Common/ResourceDisposer.cs
+++ src/RawRabbit/Common/ResourceDisposer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using RabbitMQ.Client;
+using RawRabbit.Channel;
 using RawRabbit.Channel.Abstraction;
 using RawRabbit.Configuration;
 using RawRabbit.Subscription;
@@ -17,17 +18,20 @@ namespace RawRabbit.Common
 		private readonly IChannelFactory _channelFactory;
 		private readonly IConnectionFactory _connectionFactory;
 		private readonly ISubscriptionRepository _subscriptionRepo;
+		private readonly IChannelPoolFactory _channelPoolFactory;
 		private readonly RawRabbitConfiguration _config;
 
 		public ResourceDisposer(
 			IChannelFactory channelFactory,
 			IConnectionFactory connectionFactory,
 			ISubscriptionRepository subscriptionRepo,
+			IChannelPoolFactory channelPoolFactory,
 			RawRabbitConfiguration config)
 		{
 			_channelFactory = channelFactory;
 			_connectionFactory = connectionFactory;
 			_subscriptionRepo = subscriptionRepo;
+			_channelPoolFactory = channelPoolFactory;
 			_config = config;
 		}
 
@@ -35,6 +39,7 @@ namespace RawRabbit.Common
 		{
 			_channelFactory.Dispose();
 			(_connectionFactory as IDisposable)?.Dispose();
+			(_channelPoolFactory as IDisposable)?.Dispose();
 		}
 
 		public async Task ShutdownAsync(TimeSpan? graceful = null)
