94a6fd9b09ab10cdd259e054daa0f525e45221ac
Pär Dahlman <par.dahlman@fellowdeveloper.se>
2018-02-17 18:01:03 +0100

    (#123) Tabs to spaces

--- src/RawRabbit.Enrichers.ZeroFormatter/ZeroFormatterPlugin.cs
+++ src/RawRabbit.Enrichers.ZeroFormatter/ZeroFormatterPlugin.cs
@@ -3,17 +3,17 @@ using RawRabbit.Serialization;
 
 namespace RawRabbit.Enrichers.ZeroFormatter
 {
-    public static class ZeroFormatterPlugin
-    {
-        /// <summary>
-        /// Replaces the default serializer with ZeroFormatter.
-        /// </summary>
-        public static IClientBuilder UseZeroFormatter(this IClientBuilder builder)
-        {
-            builder.Register(
-                pipe: p => { },
-                ioc: di => di.AddSingleton<ISerializer, ZeroFormatterSerializerWorker>());
-            return builder;
-        }
-    }
+	public static class ZeroFormatterPlugin
+	{
+		/// <summary>
+		/// Replaces the default serializer with ZeroFormatter.
+		/// </summary>
+		public static IClientBuilder UseZeroFormatter(this IClientBuilder builder)
+		{
+			builder.Register(
+				pipe: p => { },
+				ioc: di => di.AddSingleton<ISerializer, ZeroFormatterSerializerWorker>());
+			return builder;
+		}
+	}
 }
